b2273e34a7a9b1eb01fea31d72f19ad444546614
rdx <31172722+rd-xx@users.noreply.github.com>
2023-08-17 13:43:08 +0200

    feat(ui): add themes

---
+++ apps/nextjs/src/app/settings/page.tsx
@@ -0,0 +1,10 @@
+import ThemeSwitch from "./_components/ThemeSwitch";
+
+export default function SettingsPage() {
+  return (
+    <div>
+      <span>Settings</span>
+      <ThemeSwitch />
+    </div>
+  );
+}
