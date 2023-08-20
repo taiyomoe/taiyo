fb8fb12341da65e87cc0a28b0f044a5a5dac1873
rdx <31172722+rd-xx@users.noreply.github.com>
2023-08-20 16:13:10 +0200

    feat(db): add clear script

--- packages/db/seed.ts
+++ packages/db/seed.ts
@@ -16,25 +16,30 @@ import user1 from "./seed/user-1";
 
 if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL is not set");
+} else if (!process.env.NODE_ENV) {
+  throw new Error("NODE_ENV is not set");
 }
 
 const db = drizzle(neon(process.env.DATABASE_URL!));
 
 async function seed() {
-  console.log("Seeding database...");
+  console.log("Seeding database.....");
 
   await trackers.execute(db).then(() => console.log("Trackers seeded"));
-  await user1.execute(db).then(() => console.log("User 1 seeded"));
-  await media1.execute(db).then(() => console.log("Media 1 seeded"));
-  await media2.execute(db).then(() => console.log("Media 2 seeded"));
-  await media3.execute(db).then(() => console.log("Media 3 seeded"));
-  await media4.execute(db).then(() => console.log("Media 4 seeded"));
-  await media5.execute(db).then(() => console.log("Media 5 seeded"));
-  await media6.execute(db).then(() => console.log("Media 6 seeded"));
-  await media7.execute(db).then(() => console.log("Media 7 seeded"));
-  await media8.execute(db).then(() => console.log("Media 8 seeded"));
-  await media9.execute(db).then(() => console.log("Media 9 seeded"));
-  await media10.execute(db).then(() => console.log("Media 10 seeded"));
+
+  if (process.env.NODE_ENV === "development") {
+    await user1.execute(db).then(() => console.log("User 1 seeded"));
+    await media1.execute(db).then(() => console.log("Media 1 seeded"));
+    await media2.execute(db).then(() => console.log("Media 2 seeded"));
+    await media3.execute(db).then(() => console.log("Media 3 seeded"));
+    await media4.execute(db).then(() => console.log("Media 4 seeded"));
+    await media5.execute(db).then(() => console.log("Media 5 seeded"));
+    await media6.execute(db).then(() => console.log("Media 6 seeded"));
+    await media7.execute(db).then(() => console.log("Media 7 seeded"));
+    await media8.execute(db).then(() => console.log("Media 8 seeded"));
+    await media9.execute(db).then(() => console.log("Media 9 seeded"));
+    await media10.execute(db).then(() => console.log("Media 10 seeded"));
+  }
 }
 
 seed().catch((e) => {
