38910bd34ca13dac94030dc3159751c720cdf586
rdx <31172722+rd-xx@users.noreply.github.com>
2023-08-20 15:01:11 +0200

    refactor(db): remove unused imports

--- packages/db/schema/mediaTags.ts
+++ packages/db/schema/mediaTags.ts
@@ -1,4 +1,4 @@
-import { pgTable, text, timestamp, uuid, varchar } from "drizzle-orm/pg-core";
+import { pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
 
 import { medias } from "./medias";
 
