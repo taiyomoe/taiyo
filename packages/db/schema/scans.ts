e6950c28b8e9e4a10f9687b244ebc4b849b3eb9e
rdx <31172722+rd-xx@users.noreply.github.com>
2023-08-20 15:01:11 +0200

    refactor(db): remove unused imports

--- packages/db/schema/scans.ts
+++ packages/db/schema/scans.ts
@@ -1,5 +1,5 @@
 import { relations } from "drizzle-orm";
-import { pgTable, text, timestamp, uuid, varchar } from "drizzle-orm/pg-core";
+import { pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
 
 import { scanMembers } from "./scanMembers";
 
