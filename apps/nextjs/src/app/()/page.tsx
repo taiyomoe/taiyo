1414ece23b885f71f7c6995a2a885a4afc2e3a6a
rdx <31172722+rd-xx@users.noreply.github.com>
2023-08-22 01:53:08 +0200

    feat(home): show basic user info for debugging purposes

--- apps/nextjs/src/app/()/page.tsx
+++ apps/nextjs/src/app/()/page.tsx
@@ -12,9 +12,18 @@ export default async function HomePage() {
   return (
     <div>
       <p className="text-4xl">Taiyō</p>
-      <p>{session?.user.id}</p>
-      <p>{session?.user.name}</p>
-      <p>{session?.user.email}</p>
+      <div className="mt-8 flex gap-4">
+        <p className="min-w-[100px]">Id</p>
+        <p>{session?.user.id}</p>
+      </div>
+      <div className="flex gap-4">
+        <p className="min-w-[100px]">Username</p>
+        <p>{session?.user.name}</p>
+      </div>
+      <div className="mb-10 flex gap-4">
+        <p className="min-w-[100px]">Email</p>
+        <p>{session?.user.email}</p>
+      </div>
       <Image
         src={session?.user?.image ?? ""}
         width={150}
