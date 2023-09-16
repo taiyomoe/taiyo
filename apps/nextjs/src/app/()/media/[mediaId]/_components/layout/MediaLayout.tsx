fc00cf3a0d43181a638011128a72e59c8d4be622
rdx <31172722+rd-xx@users.noreply.github.com>
2023-09-16 17:38:28 +0200

    fix(layout): media layout grid column width

--- apps/nextjs/src/app/()/media/[mediaId]/_components/layout/MediaLayout.tsx
+++ apps/nextjs/src/app/()/media/[mediaId]/_components/layout/MediaLayout.tsx
@@ -13,21 +13,24 @@ export const MediaLayout = ({ media, children }: Props) => {
   const bannerUrl = MediaUtils.getBannerUrl(media);
 
   return (
-    <main className=" grid h-full grid-cols-3">
+    <main className="lg:grid-cols-lgMediaLayout xl:grid-cols-xlMediaLayout 2xl:grid-cols-2xlMediaLayout grid h-full grid-cols-2">
       <Image
         src={bannerUrl}
         width={0}
         height={0}
         style={{ width: "100%", height: "300px", objectFit: "cover" }}
         alt="media's banner"
+        skeletonProps={{
+          className: "rounded-none",
+        }}
         classes={{
-          container: "col-span-3 -z-50",
+          container: "col-span-2 -z-50",
         }}
       />
-      <div className="col-span-3 -mt-36 flex h-fit justify-center bg-amber-900 bg-opacity-50 lg:col-span-1">
+      <div className="col-span-2 -mt-36 flex h-fit w-full justify-center bg-amber-900 bg-opacity-50 lg:col-span-1">
         <DisplayMediaCover media={media} />
       </div>
-      <div className="col-span-3 bg-green-900 bg-opacity-50 lg:col-span-2 lg:-mt-36">
+      <div className="col-span-2 w-full bg-green-900 bg-opacity-50 lg:col-span-1 lg:-mt-36">
         {children}
       </div>
     </main>
