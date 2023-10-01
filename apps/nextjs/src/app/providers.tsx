6b41fdea9f316554035dbf3cf92dcbb5b842e9d6
rdx <31172722+rd-xx@users.noreply.github.com>
2023-10-01 22:25:06 +0200

    fix(types): update tRPC in every package

--- apps/nextjs/src/app/providers.tsx
+++ apps/nextjs/src/app/providers.tsx
@@ -5,19 +5,19 @@ import { NextUIProvider } from "@nextui-org/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { ReactQueryStreamedHydration } from "@tanstack/react-query-next-experimental";
-// import { loggerLink, unstable_httpBatchStreamLink } from "@trpc/client";
+import { loggerLink, unstable_httpBatchStreamLink } from "@trpc/client";
 import { ThemeProvider } from "next-themes";
 import superjson from "superjson";
 
-// import { env } from "~/env.mjs";
+import { env } from "~/env.mjs";
 import { api } from "~/utils/api";
 
-// const getBaseUrl = () => {
-//   if (typeof window !== "undefined") return ""; // browser should use relative url
-//   if (env.VERCEL_URL) return env.VERCEL_URL; // SSR should use vercel url
+const getBaseUrl = () => {
+  if (typeof window !== "undefined") return ""; // browser should use relative url
+  if (env.VERCEL_URL) return env.VERCEL_URL; // SSR should use vercel url
 
-//   return `http://localhost:${env.PORT}`; // dev SSR should use localhost
-// };
+  return `http://localhost:${env.PORT}`; // dev SSR should use localhost
+};
 
 type ProviderProps = {
   children: React.ReactNode;
@@ -41,19 +41,19 @@ const TRPCReactProvider = (props: ProviderProps) => {
     api.createClient({
       transformer: superjson,
       links: [
-        // loggerLink({
-        //   enabled: (opts) =>
-        //     process.env.NODE_ENV === "development" ||
-        //     (opts.direction === "down" && opts.result instanceof Error),
-        // }),
-        // unstable_httpBatchStreamLink({
-        //   url: `${getBaseUrl()}/api/trpc`,
-        //   headers() {
-        //     const headers = new Map(props.headers);
-        //     headers.set("x-trpc-source", "nextjs-react");
-        //     return Object.fromEntries(headers);
-        //   },
-        // }),
+        loggerLink({
+          enabled: (opts) =>
+            process.env.NODE_ENV === "development" ||
+            (opts.direction === "down" && opts.result instanceof Error),
+        }),
+        unstable_httpBatchStreamLink({
+          url: `${getBaseUrl()}/api/trpc`,
+          headers() {
+            const headers = new Map(props.headers);
+            headers.set("x-trpc-source", "nextjs-react");
+            return Object.fromEntries(headers);
+          },
+        }),
       ],
     }),
   );
