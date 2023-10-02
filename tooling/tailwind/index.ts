74fcb52b66e09e1a5dfb246fda5071f96068e1f5
rdx <31172722+rd-xx@users.noreply.github.com>
2023-10-02 14:14:14 +0200

    fix(tailwind): remove useless package & code

--- tooling/tailwind/index.ts
+++ tooling/tailwind/index.ts
@@ -52,31 +52,7 @@ export default {
           foreground: "hsl(var(--card-foreground))",
         },
       },
-      borderRadius: {
-        lg: "var(--radius)",
-        md: "calc(var(--radius) - 2px)",
-        sm: "calc(var(--radius) - 4px)",
-      },
-      keyframes: {
-        "accordion-down": {
-          from: { height: "0px" },
-          to: { height: "var(--radix-accordion-content-height)" },
-        },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: "0px" },
-        },
-      },
-      animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
-      },
-      gridTemplateColumns: {
-        lgMediaLayout: "350px auto",
-        xlMediaLayout: "420px auto",
-        "2xlMediaLayout": "550px auto",
-      },
     },
   },
-  plugins: [require("tailwindcss-animate"), require("tailwind-scrollbar")],
+  plugins: [require("tailwind-scrollbar")],
 } satisfies Config;
