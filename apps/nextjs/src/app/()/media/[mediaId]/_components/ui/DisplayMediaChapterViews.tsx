e6719d149008b319f53f04d6ccd629e5aaf17a1b
rdx <31172722+rd-xx@users.noreply.github.com>
2023-10-03 23:55:09 +0200

    fix(ui): change bg color of icons

--- apps/nextjs/src/app/()/media/[mediaId]/_components/ui/DisplayMediaChapterViews.tsx
+++ apps/nextjs/src/app/()/media/[mediaId]/_components/ui/DisplayMediaChapterViews.tsx
@@ -3,7 +3,7 @@ import { EyeIcon } from "lucide-react";
 export const DisplayMediaChapterViews = () => {
   return (
     <div className="flex w-full items-center justify-end gap-1 bg-green-100 md:justify-start">
-      <EyeIcon size={20} />
+      <EyeIcon className="bg-red-100" size={20} />
       <p className="select-none rounded bg-blue-100 px-1 text-sm">N/A</p>
     </div>
   );
