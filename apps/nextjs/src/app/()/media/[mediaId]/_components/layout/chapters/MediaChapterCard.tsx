4165cea18193a221d1e5025799d3baab6004186f
rdx <31172722+rd-xx@users.noreply.github.com>
2023-10-03 22:48:41 +0200

    feat(media): fix media chapter cards layout

--- apps/nextjs/src/app/()/media/[mediaId]/_components/layout/chapters/MediaChapterCard.tsx
+++ apps/nextjs/src/app/()/media/[mediaId]/_components/layout/chapters/MediaChapterCard.tsx
@@ -6,7 +6,10 @@ import type { MediaChapterWithUser } from "@taiyo/db";
 
 import { cn } from "~/utils/cn";
 import { MediaChapterUtils } from "~/utils/MediaChapterUtils";
+import { DisplayMediaChapterCommentsCount } from "../../ui/DisplayMediaChapterCommentsCount";
+import { DisplayMediaChapterUploadedTime } from "../../ui/DisplayMediaChapterUploadedTime";
 import { DisplayMediaChapterUploader } from "../../ui/DisplayMediaChapterUploader";
+import { DisplayMediaChapterViews } from "../../ui/DisplayMediaChapterViews";
 import { MediaChapterCardPath } from "./MediaChapterCardPath";
 
 type Props = {
@@ -28,7 +31,7 @@ export const MediaChapterCard = ({ chapter, order }: Props) => {
         <CardBody className="p-3">
           <div className="grid-cols-mediaChapterCardLayout grid grid-rows-3 gap-1 md:grid-rows-2">
             <Link
-              className="col-span-10 md:col-span-9"
+              className="order-1 col-span-10 md:col-span-9"
               href={MediaChapterUtils.getUrl(chapter)}
             >
               <p className="w-full truncate text-sm font-semibold">
@@ -36,24 +39,24 @@ export const MediaChapterCard = ({ chapter, order }: Props) => {
               </p>
             </Link>
             {/* UPLOADED TIME */}
-            <div className="col-span-2">
-              <Skeleton className="h-full w-full rounded" />
+            <div className="order-6 col-span-4 md:order-2 md:col-span-2">
+              <DisplayMediaChapterUploadedTime chapter={chapter} />
             </div>
             {/* VIEWS */}
-            <div className="col-span-9 md:col-span-1">
-              <Skeleton className="h-full w-full rounded" />
+            <div className="order-4 col-span-3 md:order-3 md:col-span-1">
+              <DisplayMediaChapterViews />
             </div>
             {/* SCANS */}
-            <div className="col-span-3 md:col-span-9">
+            <div className="order-3 col-span-9 md:order-4">
               <Skeleton className="h-full w-full rounded" />
             </div>
             {/* UPLOADER */}
-            <div className="col-span-8 md:col-span-2">
+            <div className="order-5 col-span-8 md:col-span-2">
               <DisplayMediaChapterUploader user={chapter.user} />
             </div>
             {/* COMMENTS */}
-            <div className="col-span-4 md:col-span-1">
-              <Skeleton className="h-full w-full rounded" />
+            <div className="order-2 col-span-2 md:order-6 md:col-span-1">
+              <DisplayMediaChapterCommentsCount />
             </div>
           </div>
         </CardBody>
---
+++ apps/nextjs/src/app/()/media/[mediaId]/_components/ui/DisplayMediaChapterUploadedTime.tsx
@@ -0,0 +1,20 @@
+import { ClockIcon } from "lucide-react";
+
+import type { MediaChapter } from "@taiyo/db";
+
+import { MediaChapterUtils } from "~/utils/MediaChapterUtils";
+
+type Props = {
+  chapter: MediaChapter;
+};
+
+export const DisplayMediaChapterUploadedTime = ({ chapter }: Props) => {
+  return (
+    <div className="flex w-full items-center justify-end gap-1 bg-green-100 md:justify-start">
+      <ClockIcon size={20} />
+      <p className="select-none rounded bg-blue-100 px-1 text-sm md:px-2">
+        {MediaChapterUtils.computeUploadedTime(chapter)}
+      </p>
+    </div>
+  );
+};
