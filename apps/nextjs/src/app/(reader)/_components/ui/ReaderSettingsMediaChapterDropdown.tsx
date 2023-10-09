df7f1570af9e3aa6a259b41dc3d91280509029dd
Vitor Sousa <vitor.gomes@particeep.com>
2023-10-09 10:25:26 +0200

    feat(reader): add scrollbars to dropdown menus

--- apps/nextjs/src/app/(reader)/_components/ui/ReaderSettingsMediaChapterDropdown.tsx
+++ apps/nextjs/src/app/(reader)/_components/ui/ReaderSettingsMediaChapterDropdown.tsx
@@ -14,28 +14,59 @@ import {
   ChevronRightIcon,
   ChevronsUpDownIcon,
 } from "lucide-react";
+import { tv } from "tailwind-variants";
 
 import { mediaChapterAtom } from "~/atoms/mediaChapter.atoms";
 
+const readerSettingsMediaChapterDropdown = tv({
+  slots: {
+    container: "flex w-full gap-2",
+    navigationButton: "h-full",
+    triggerButton: "h-full justify-between py-2 pr-2",
+    skeleton: "h-9 w-full rounded-lg",
+    dropdownBase: "",
+    dropdownMenu: "",
+  },
+  variants: {
+    scollable: {
+      true: {
+        dropdownBase: "rounded-r-none p-0",
+        dropdownMenu:
+          "scrollbar-track-content3 scrollbar-thumb-rounded-md max-h-[300px] overflow-y-scroll p-2 scrollbar-thin scrollbar-thumb-primary",
+      },
+    },
+  },
+});
+
 export const ReaderSettingsMediaChapterDropdown = () => {
   const chapter = useAtomValue(mediaChapterAtom);
+  const {
+    container,
+    navigationButton,
+    triggerButton,
+    skeleton,
+    dropdownBase,
+    dropdownMenu,
+  } = readerSettingsMediaChapterDropdown({
+    scollable: false,
+  });
 
   return (
-    <div className="flex w-full gap-2">
+    <div className={container()}>
       <Button
-        className="h-full"
+        className={navigationButton()}
         startContent={<ChevronLeftIcon size={20} />}
         isDisabled={!chapter}
         radius="sm"
         size="sm"
         isIconOnly
       />
-      {!chapter && <Skeleton className="h-9 w-full rounded-lg" />}
+      {!chapter && <Skeleton className={skeleton()} />}
       {chapter && (
-        <Dropdown>
+        <Dropdown classNames={{ base: dropdownBase() }}>
           <DropdownTrigger>
             <Button
-              className="h-full justify-between py-2 pr-2"
+              className={triggerButton()}
               radius="sm"
               fullWidth
               endContent={<ChevronsUpDownIcon size={20} />}
@@ -43,7 +74,7 @@ export const ReaderSettingsMediaChapterDropdown = () => {
               Capítulo 26
             </Button>
           </DropdownTrigger>
-          <DropdownMenu>
+          <DropdownMenu className={dropdownMenu()}>
             {chapter.media.chapters.map((_, i) => (
               <DropdownItem key={i}>Capítulo {i + 1}</DropdownItem>
             ))}
@@ -51,7 +82,7 @@ export const ReaderSettingsMediaChapterDropdown = () => {
         </Dropdown>
       )}
       <Button
-        className="h-full"
+        className={navigationButton()}
         startContent={<ChevronRightIcon size={20} />}
         isDisabled={!chapter}
         radius="sm"
