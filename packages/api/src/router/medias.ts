5aa4d78eae1338052816080a21fbac79ca8a1c30
Vitor Sousa <vitor.gomes@particeep.com>
2023-10-09 02:24:02 +0200

    fix(media): remove chapter-related function from media router

--- packages/api/src/router/medias.ts
+++ packages/api/src/router/medias.ts
@@ -2,23 +2,11 @@ import { z } from "zod";
 
 import { db } from "@taiyo/db";
 import type { MediasWithCovers, MediaWithRelations } from "@taiyo/db/types";
-import type { MediaChapterWithRelations } from "@taiyo/db/types/mediaChapter.types";
 
 import { createTRPCRouter, publicProcedure } from "../trpc";
 
 export const mediasRouter = createTRPCRouter({
-  getLatestMedias: publicProcedure.query(async () => {
-    const result = await db.query.medias.findMany({
-      with: {
-        covers: true,
-      },
-      limit: 20,
-      orderBy: (m, { desc }) => desc(m.createdAt),
-    });
-
-    return result as MediasWithCovers;
-  }),
-  getMediaById: publicProcedure
+  getById: publicProcedure
     .input(z.string())
     .query(async ({ input: mediaId }) => {
       const result = await db.query.medias.findFirst({
@@ -41,18 +29,15 @@ export const mediasRouter = createTRPCRouter({
 
       return result as MediaWithRelations;
     }),
-  getMediaChapterById: publicProcedure
-    .input(z.string())
-    .query(async ({ input: chapterId }) => {
-      const result = await db.query.mediaChapters.findFirst({
-        where: (c, { eq }) => eq(c.id, chapterId),
-        with: {
-          media: true,
-          comments: true,
-          user: true,
-        },
-      });
+  getLatestMedias: publicProcedure.query(async () => {
+    const result = await db.query.medias.findMany({
+      with: {
+        covers: true,
+      },
+      limit: 20,
+      orderBy: (m, { desc }) => desc(m.createdAt),
+    });
 
-      return result as MediaChapterWithRelations;
-    }),
+    return result as MediasWithCovers;
+  }),
 });
