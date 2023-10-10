c175bc916a8f765ed93c7a8f287709209bbd4d9d
Vitor Sousa <vitor.gomes@particeep.com>
2023-10-11 00:38:12 +0200

    feat(chapters): add page navigation

--- apps/nextjs/src/app/(reader)/chapter/[chapterId]/_components/DisplayMediaChapterPage.tsx
+++ apps/nextjs/src/app/(reader)/chapter/[chapterId]/_components/DisplayMediaChapterPage.tsx
@@ -1,49 +1,21 @@
 "use client";
 
-import { useEffect, useState } from "react";
 import Image from "next/image";
-import { usePathname } from "next/navigation";
-import { useAtomValue } from "jotai";
 
-import type { MediaChapterLimited } from "@taiyo/db/types";
+import { useChapterNavigation } from "~/hooks/useChapterNavigation";
 
-import { mediaChapterNavigationAtom } from "~/atoms/mediaChapter.atoms";
-import { MediaChapterUtils } from "~/utils/MediaChapterUtils";
-
-type Props = {
-  mediaChapter: MediaChapterLimited;
-};
-
-export const DisplayMediaChapterPage = ({ mediaChapter }: Props) => {
-  const chapterNavigation = useAtomValue(mediaChapterNavigationAtom);
-  const pathname = usePathname();
-  const { rawPathname } = MediaChapterUtils.parseUrl(pathname);
-  const [currentPageNumber, setCurrentPageNumber] = useState(
-    chapterNavigation?.currentPage,
-  );
-
-  const currentPageUrl = MediaChapterUtils.getCurrentPageUrl(
-    mediaChapter,
-    chapterNavigation,
-  );
-
-  const handleForwardClick = () => {
-    if (chapterNavigation?.nextPage) {
-      setCurrentPageNumber(chapterNavigation.nextPage);
-    }
-  };
-
-  useEffect(() => {
-    if (!currentPageNumber) {
-      return;
-    }
-
-    window.history.pushState({}, "", `${rawPathname}/${currentPageNumber}`);
-  }, [currentPageNumber, rawPathname]);
+export const DisplayMediaChapterPage = () => {
+  const { currentPageUrl, goBack, goForward } = useChapterNavigation();
 
   return (
     <div className="relative flex h-[calc(100%-61px)] bg-pink-800">
-      <div className="z-10 h-full w-1/2 bg-blue-800 bg-opacity-50" />
+      <div
+        className="z-10 h-full w-1/2 bg-blue-800 bg-opacity-50"
+        onClick={goBack}
+        onKeyDown={goBack}
+        role="button"
+        tabIndex={0}
+      />
       {currentPageUrl && (
         <Image
           src={currentPageUrl}
@@ -56,8 +28,8 @@ export const DisplayMediaChapterPage = ({ mediaChapter }: Props) => {
       )}
       <div
         className="z-10 h-full w-1/2 bg-green-800 bg-opacity-50 hover:cursor-pointer focus-visible:outline-none"
-        onClick={handleForwardClick}
-        onKeyDown={handleForwardClick}
+        onClick={goForward}
+        onKeyDown={goForward}
         role="button"
         tabIndex={0}
       />
