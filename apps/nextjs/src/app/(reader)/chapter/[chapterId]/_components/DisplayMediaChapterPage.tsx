e347f2389e3b18d3ec1d60bf7cada99138b59033
rdx <reydux95@gmail.com>
2023-10-11 10:39:07 +0200

    feat(chapters): add keyboard navigation

--- apps/nextjs/src/app/(reader)/chapter/[chapterId]/_components/DisplayMediaChapterPage.tsx
+++ apps/nextjs/src/app/(reader)/chapter/[chapterId]/_components/DisplayMediaChapterPage.tsx
@@ -1,20 +1,34 @@
 "use client";
 
+import { useRef } from "react";
 import Image from "next/image";
+import { useKeyPress } from "@uidotdev/usehooks";
 
 import { useChapterNavigation } from "~/hooks/useChapterNavigation";
 
 export const DisplayMediaChapterPage = () => {
   const { currentPageUrl, goBack, goForward } = useChapterNavigation();
+  const backButtonRef = useRef<HTMLButtonElement>(null);
+  const forwardButtonRef = useRef<HTMLButtonElement>(null);
+
+  const handleKeyPress = (e: KeyboardEvent) => {
+    if (e.key === "ArrowLeft") {
+      backButtonRef.current?.click();
+      return;
+    }
+
+    forwardButtonRef.current?.click();
+  };
+
+  useKeyPress("ArrowLeft", handleKeyPress);
+  useKeyPress("ArrowRight", handleKeyPress);
 
   return (
     <div className="relative flex h-[calc(100%-61px)] bg-pink-800">
-      <div
-        className="z-10 h-full w-1/2 bg-blue-800 bg-opacity-50"
+      <button
+        ref={backButtonRef}
+        className="z-10 h-full w-1/2 bg-blue-800 bg-opacity-50 hover:cursor-pointer focus-visible:outline-none"
         onClick={goBack}
-        onKeyDown={goBack}
-        role="button"
-        tabIndex={0}
       />
       {currentPageUrl && (
         <Image
@@ -27,12 +41,10 @@ export const DisplayMediaChapterPage = () => {
           fill
         />
       )}
-      <div
-        className="z-10 h-full w-1/2 bg-green-800 bg-opacity-50 hover:cursor-pointer focus-visible:outline-none"
+      <button
+        ref={forwardButtonRef}
+        className="z-10 h-full w-1/2 bg-green-800 bg-opacity-50 focus-visible:outline-none"
         onClick={goForward}
-        onKeyDown={goForward}
-        role="button"
-        tabIndex={0}
       />
     </div>
   );
