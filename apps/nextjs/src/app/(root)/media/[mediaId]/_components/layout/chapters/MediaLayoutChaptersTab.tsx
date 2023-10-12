af324ad1b86b53bcd2a80795aa90c5824c246db4
rdx <reydux95@gmail.com>
2023-10-12 23:08:13 +0200

    feat(media): add expand/collapse button

--- apps/nextjs/src/app/(root)/media/[mediaId]/_components/layout/chapters/MediaLayoutChaptersTab.tsx
+++ apps/nextjs/src/app/(root)/media/[mediaId]/_components/layout/chapters/MediaLayoutChaptersTab.tsx
@@ -1,11 +1,15 @@
 "use client";
 
+import { useState } from "react";
 import { Accordion, AccordionItem } from "@nextui-org/accordion";
+import { Divider } from "@nextui-org/divider";
 
 import type { MediaLimited } from "@taiyo/db/types";
 
 import { MediaChapterUtils } from "~/utils/MediaChapterUtils";
 import { MediaChapterGroupCard } from "./MediaChapterGroupCard";
+import { MediaChaptersTabActions } from "./MediaChaptersTabActions";
+import { MediaChaptersTabPagination } from "./MediaChaptersTabPagination";
 
 type Props = {
   media: MediaLimited;
@@ -16,38 +20,67 @@ export const MediaLayoutChaptersTab = ({ media }: Props) => {
   const volumeKeys = computedVolumes.map(({ volume }) => `volume-${volume}`);
   const groupKeys = media.chapters.map((chapter) => `group-${chapter.number}`);
 
+  const [selectedVolumeKeys, setSelectedVolumeKeys] = useState(
+    new Set(volumeKeys),
+  );
+  const [selectedGroupKeys, setSelectedGroupKeys] = useState(
+    new Set(groupKeys),
+  );
+
   const volumeAccordionTitle = (volume: string) => `Volume ${volume}`;
   const chapterAccordionTitle = (chapter: number) => `Capítulo ${chapter}`;
 
+  const collapseVolumes = () => {
+    setSelectedVolumeKeys(new Set());
+    setSelectedGroupKeys(new Set());
+  };
+  const expandVolumes = () => {
+    setSelectedVolumeKeys(new Set(volumeKeys));
+  };
+
   return (
-    <Accordion
-      className="px-0"
-      selectionMode="multiple"
-      defaultExpandedKeys={volumeKeys}
-    >
-      {computedVolumes.map(({ volume, groups }) => (
-        <AccordionItem
-          key={`volume-${volume}`}
-          title={volumeAccordionTitle(volume)}
-        >
-          <Accordion
-            className="px-0"
-            selectionMode="multiple"
-            defaultExpandedKeys={groupKeys}
-            isCompact
+    <div className="flex flex-col gap-2">
+      <MediaChaptersTabActions
+        collapseVolumes={collapseVolumes}
+        expandVolumes={expandVolumes}
+      />
+      <Accordion
+        className="px-0"
+        selectionMode="multiple"
+        selectedKeys={selectedVolumeKeys}
+        defaultExpandedKeys={volumeKeys}
+        // @ts-expect-error -- NextUI wrong types
+        onSelectionChange={setSelectedVolumeKeys}
+      >
+        {computedVolumes.map(({ volume, groups }) => (
+          <AccordionItem
+            key={`volume-${volume}`}
+            title={volumeAccordionTitle(volume)}
           >
-            {groups.map((group, i) => (
-              <AccordionItem
-                key={`group-${group.number}`}
-                title={chapterAccordionTitle(group.number)}
+            <Accordion
+              className="px-0"
+              selectionMode="multiple"
+              selectedKeys={selectedGroupKeys}
+              defaultExpandedKeys={groupKeys}
+              // @ts-expect-error -- NextUI wrong types
+              onSelectionChange={setSelectedGroupKeys}
+              isCompact
+            >
+              {groups.map((group, i) => (
+                <AccordionItem
+                  key={`group-${group.number}`}
+                  title={chapterAccordionTitle(group.number)}
                   classNames={{ title: "text-sm" }}
-              >
-                <MediaChapterGroupCard key={i} group={group} />
-              </AccordionItem>
-            ))}
-          </Accordion>
-        </AccordionItem>
-      ))}
-    </Accordion>
+                >
+                  <MediaChapterGroupCard key={i} group={group} />
+                </AccordionItem>
+              ))}
+            </Accordion>
+          </AccordionItem>
+        ))}
+      </Accordion>
+      <Divider className="my-4" />
+      <MediaChaptersTabPagination />
+    </div>
   );
 };
