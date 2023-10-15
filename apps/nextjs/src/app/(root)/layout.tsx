6c90ab3253f1a5472e2dac7c90fc3575015f72c1
rdx <reydux95@gmail.com>
2023-10-15 12:15:28 +0200

    fix(navbar): use navbarauth instead

--- apps/nextjs/src/app/(root)/layout.tsx
+++ apps/nextjs/src/app/(root)/layout.tsx
@@ -1,10 +1,10 @@
-import { Navbar } from "~/components/navbar/Navbar";
+import { NavbarAuth } from "~/components/navbar/NavbarAuth";
 import type { LayoutProps } from "~/types";
 
 export default function Layout({ children }: LayoutProps) {
   return (
     <div className="flex h-full flex-col">
-      <Navbar />
+      <NavbarAuth />
       {children}
     </div>
   );
