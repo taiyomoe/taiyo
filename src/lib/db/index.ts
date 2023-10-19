ae24e51411cf1645b2514542949bc222ba29c83e
rdx <reydux95@gmail.com>
2023-10-19 16:34:47 +0200

    refactor(db): start replacing Drizzle with Prisma

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,43 +1,16 @@
-import { neon, neonConfig } from "@neondatabase/serverless";
-import { drizzle } from "drizzle-orm/neon-http";
+import { PrismaClient } from "@prisma/client";
 
-import * as mediaBanners from "./schema/mediaBanners";
-import * as mediaChapterComments from "./schema/mediaChapterComments";
-import * as mediaChapters from "./schema/mediaChapters";
-import * as mediaChapterScans from "./schema/mediaChapterScans";
-import * as mediaCovers from "./schema/mediaCovers";
-import * as medias from "./schema/medias";
-import * as mediaTags from "./schema/mediaTags";
-import * as mediaTitles from "./schema/mediaTitles";
-import * as mediaTrackers from "./schema/mediaTrackers";
-import * as scanMembers from "./schema/scanMembers";
-import * as scans from "./schema/scans";
-import * as tags from "./schema/tags";
-import * as trackers from "./schema/trackers";
-import * as users from "./schema/users";
+import { env } from "~/lib/env.mjs";
 
-neonConfig.fetchConnectionCache = true;
-
-export const schema = {
-  ...users,
-  ...tags,
-  ...trackers,
-  ...medias,
-  ...mediaCovers,
-  ...mediaBanners,
-  ...mediaTags,
-  ...mediaTitles,
-  ...mediaChapters,
-  ...mediaChapterScans,
-  ...mediaChapterComments,
-  ...mediaTrackers,
-  ...scans,
-  ...scanMembers,
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
 };
 
-export * from "drizzle-orm";
+export const db =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log:
+      env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
+  });
 
-export const db = drizzle(neon(process.env.DATABASE_URL!), {
-  schema,
-  logger: true,
-});
+if (env.NODE_ENV !== "production") globalForPrisma.prisma = db;
