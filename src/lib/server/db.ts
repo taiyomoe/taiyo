cdaff1f1d8a19ed801159d5036867ce1524b6406
rdx <reydux95@gmail.com>
2023-10-24 11:03:50 +0200

    refactor(db): remove Prisma Accelerate

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -1,20 +1,16 @@
-import { PrismaClient } from "@prisma/client/edge";
-import { withAccelerate } from "@prisma/extension-accelerate";
+import { PrismaClient } from "@prisma/client";
 
 import { env } from "~/lib/env.mjs";
 
-const getExtendedClient = () => {
-  return new PrismaClient({
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
+export const db =
+  globalForPrisma.prisma ??
+  new PrismaClient({
     log:
       env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
-  }).$extends(withAccelerate());
-};
-export type ExtendedPrismaClient = ReturnType<typeof getExtendedClient>;
-
-const globalForPrisma = globalThis as unknown as {
-  prisma: ExtendedPrismaClient | undefined;
-};
-
-export const db = globalForPrisma.prisma ?? getExtendedClient();
+  });
 
 if (env.NODE_ENV !== "production") globalForPrisma.prisma = db;
