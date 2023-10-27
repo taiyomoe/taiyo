8c62e566181bf4a16cee34b434c13399c058d4c5
rdx <reydux95@gmail.com>
2023-10-27 10:31:22 +0200

    feat(uploads): improve UI

--- src/components/ui/images/ImageCard.tsx
+++ src/components/ui/images/ImageCard.tsx
@@ -1,45 +1,81 @@
-import { Spinner } from "@nextui-org/react";
+import { Spinner, tv } from "@nextui-org/react";
 import prettyBytes from "pretty-bytes";
 
 import { type SelectedImage } from "~/lib/types";
-import { cn } from "~/utils/cn";
 
 type Props = {
   position: string;
 } & SelectedImage;
 
+const imageCard = tv({
+  slots: {
+    container: "relative flex max-w-full rounded-medium bg-default-200",
+    spinner: "absolute left-[calc(50%-20px)] top-[calc(50%-20px)] hidden",
+    imagePreview:
+      "h-[150px] w-[100px] rounded-l-medium object-cover md:w-[150px]",
+    contentContainer: "flex grow flex-col gap-2 p-2",
+    upperContentContainer: "flex justify-between gap-2",
+    lowerContentContainer: "flex grow flex-col justify-end gap-1",
+    fileNameText: "line-clamp-1 h-fit text-ellipsis",
+    filePositionText: "px-2",
+    fileSizeText: "w-fit rounded-lg px-2 py-1 bg-warning-200",
+    fileTypeText: "w-fit rounded-lg px-2 py-1",
+  },
+  variants: {
+    status: {
+      pending: {},
+      compressing: {
+        container: "opacity-10",
+        spinner: "block",
+      },
+      compressed: {
+        fileSizeText: "bg-success-200",
+      },
+    },
+    isJpeg: {
+      true: {
+        fileTypeText: "bg-success-200",
+      },
+      false: {
+        fileTypeText: "bg-danger-200",
+      },
+    },
+  },
+});
+
 export const ImageCard = ({ file, status, position }: Props) => {
+  const {
+    container,
+    spinner,
+    imagePreview,
+    contentContainer,
+    upperContentContainer,
+    lowerContentContainer,
+    fileNameText,
+    filePositionText,
+    fileSizeText,
+    fileTypeText,
+  } = imageCard({
+    status,
+    isJpeg: file.type === "image/jpeg",
+  });
+
   return (
-    <div
-      className={cn(
-        "relative flex max-w-full gap-2 rounded-medium bg-default-200",
-        { "opacity-10": status === "compressing" },
-      )}
-    >
-      <Spinner
-        className={cn(
-          "absolute left-[calc(50%-20px)] top-[calc(50%-20px)] hidden",
-          {
-            block: status === "compressing",
-          },
-        )}
-        size="lg"
-      />
+    <div className={container()}>
+      <Spinner className={spinner()} size="lg" />
       <img
-        className="h-[150px] w-[100px] rounded-l-medium object-cover md:w-[150px]"
+        className={imagePreview()}
         src={URL.createObjectURL(file)}
         alt={`image preview`}
       />
-      <div className="flex grow flex-col gap-2 p-2">
-        <div className="flex justify-between gap-2">
-          <p className="line-clamp-1 h-fit text-ellipsis">{file.name}</p>
-          <p className="px-2">{position}</p>
+      <div className={contentContainer()}>
+        <div className={upperContentContainer()}>
+          <p className={fileNameText()}>{file.name}</p>
+          <p className={filePositionText()}>{position}</p>
         </div>
-        <div className="flex grow justify-between">
-          <div className="flex flex-col justify-end gap-1">
-            <p>Tamanho: {prettyBytes(file.size)}</p>
-            <p>Extensão: {file.type}</p>
-          </div>
+        <div className={lowerContentContainer()}>
+          <p className={fileSizeText()}>Tamanho: {prettyBytes(file.size)}</p>
+          <p className={fileTypeText()}>Extensão: {file.type}</p>
         </div>
       </div>
     </div>
