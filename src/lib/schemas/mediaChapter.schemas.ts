c8ffa025bd90240ba6c6cb8a2851e2ab87aff14c
rdx <reydux95@gmail.com>
2023-10-27 22:16:03 +0200

    feat(chapters): improve insert schema

--- src/lib/schemas/mediaChapter.schemas.ts
+++ src/lib/schemas/mediaChapter.schemas.ts
@@ -1,21 +1,22 @@
 import { z } from "zod";
 
-import { MediaChapterSchema } from "./prisma";
+import {
+  ContentRatingSchema,
+  FlagSchema,
+  MediaChapterLanguagesSchema,
+} from "./prisma";
 
-export const insertMediaChapterSchema = MediaChapterSchema.pick({
-  id: true,
-  title: true,
-  number: true,
-  volume: true,
-  language: true,
-  pages: true,
-  contentRating: true,
-  flag: true,
-  mediaId: true,
-}).merge(
-  z.object({
-    scansIds: z.array(z.string().uuid()),
-  }),
-);
+export const insertMediaChapterSchema = z.object({
+  id: z.string().uuid(),
+  title: z.string().optional(),
+  number: z.coerce.number().min(0).default(0),
+  volume: z.coerce.number().min(0).optional(),
+  language: MediaChapterLanguagesSchema,
+  pages: z.array(z.string().uuid()),
+  contentRating: ContentRatingSchema,
+  flag: FlagSchema,
+  mediaId: z.string().uuid(),
+  scansIds: z.array(z.string().uuid()),
+});
 
 export type InsertMediaChapterSchema = typeof insertMediaChapterSchema._type;
