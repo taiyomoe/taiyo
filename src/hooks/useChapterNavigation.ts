f1d087edc0249e5ac2b8d1c5f6a4185da1765e67
rdx <reydux95@gmail.com>
2023-11-09 14:39:32 +0100

    feat(reader): automatic redirection

--- src/hooks/useChapterNavigation.ts
+++ src/hooks/useChapterNavigation.ts
@@ -1,14 +1,16 @@
 import { useCallback, useState } from "react";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import { useAtom, useAtomValue } from "jotai";
 
 import {
   mediaChapterAtom,
   mediaChapterNavigationAtom,
 } from "~/atoms/mediaChapter.atoms";
+import { MediaUtils } from "~/lib/utils/media.utils";
 import { MediaChapterUtils } from "~/lib/utils/mediaChapter.utils";
 
 export const useChapterNavigation = () => {
+  const router = useRouter();
   const pathname = usePathname();
   const { rawPathname, currentPageNumber: initialPageNumber } =
     MediaChapterUtils.parseUrl(pathname);
@@ -39,21 +41,63 @@ export const useChapterNavigation = () => {
   );
 
   const goBack = useCallback(() => {
-    if (!navigation?.previousPage || !pageNumber) {
+    if (!chapter || !navigation || !pageNumber) {
       return;
     }
 
-    goTo(navigation.previousPage);
-  }, [goTo, navigation, pageNumber]);
+    /**
+     * Go to the previous page, if there is one.
+     */
+    if (navigation.previousPage) {
+      return goTo(navigation.previousPage);
+    }
+
+    /**
+     * If there is no previous page but here is a previous chapter,
+     * then we should go to the previous chapter.
+     */
+    if (chapter.previousChapter) {
+      return router.push(MediaChapterUtils.getUrl(chapter.previousChapter));
+    }
+
+    /**
+     * If there is no previous page and no previous chapter,
+     * then we should return to the media page.
+     */
+    return router.push(MediaUtils.getUrl(chapter.media));
+  }, [chapter, goTo, navigation, pageNumber, router]);
 
   const goForward = useCallback(() => {
-    if (!navigation?.nextPage || !pageNumber) {
+    if (!chapter || !navigation || !pageNumber) {
       return;
     }
 
-    goTo(navigation.nextPage);
-  }, [goTo, navigation, pageNumber]);
+    /**
+     * Go to the next page, if there is one.
+     */
+    if (navigation.nextPage) {
+      return goTo(navigation.nextPage);
+    }
 
+    /**
+     * If there is no next page but here is a next chapter,
+     * then we should go to the next chapter.
+     */
+    if (chapter.nextChapter) {
+      return router.push(MediaChapterUtils.getUrl(chapter.nextChapter));
+    }
+
+    /**
+     * If there is no next page and no next chapter,
+     * then we should return to the media page.
+     */
+    return router.push(MediaUtils.getUrl(chapter.media));
+  }, [chapter, goTo, navigation, pageNumber, router]);
+
+  /**
+   * If the page number in the URL is invalid (either negative or too high),
+   * go to the first page.
+   */
   if (
     pageNumber &&
     chapter &&
