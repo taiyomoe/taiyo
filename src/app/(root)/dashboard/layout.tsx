220c17fa33953e9d1eb3a1867e815a488523809b
rdx <reydux95@gmail.com>
2023-11-09 19:39:36 +0100

    refactor(style): run Prettier

--- src/app/(root)/dashboard/layout.tsx
+++ src/app/(root)/dashboard/layout.tsx
@@ -5,13 +5,13 @@ import { DashboardSidebarContent } from "./_components/DashboardSidebarContent";
 
 export default function Layout({ children }: LayoutProps) {
   return (
-    <div className="min-h-reader flex h-full w-full">
+    <div className="flex h-full min-h-reader w-full">
       <Sidebar
         className="h-reader"
         title="Dashboard"
         content={<DashboardSidebarContent className="fixed -mt-[1px]" />}
       />
-      <div className="p-6 w-full">{children}</div>
+      <div className="w-full p-6">{children}</div>
     </div>
   );
 }
