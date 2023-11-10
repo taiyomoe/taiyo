47473410cf43d38e8382162aa42f3dec37ecf600
rdx <31172722+rd-xx@users.noreply.github.com>
2023-11-10 03:23:12 +0100

    Revert "feat(medias): change title color"
    This reverts commit b40bca73566b44e5cc35369b8857cc0952eece6f.

--- src/app/(root)/media/[mediaId]/_components/layout/MediaLayoutActions.tsx
+++ src/app/(root)/media/[mediaId]/_components/layout/MediaLayoutActions.tsx
@@ -9,7 +9,7 @@ type Props = {
 export const MediaLayoutActions = ({ media }: Props) => {
   return (
     <div className="grid h-28 grid-rows-2 gap-2 p-3 xl:h-36">
-      <p className="line-clamp-1 h-[44px] w-fit bg-gradient-to-br from-neutral-500 via-primary to-neutral-400 bg-clip-text text-4xl font-extrabold text-transparent [text-shadow:_0_2px_0_rgb(0_0_0_/_10%)]">
+      <p className="line-clamp-1 h-[44px] w-fit text-4xl font-bold [text-shadow:_0_3px_0_rgb(0_0_0_/_40%)]">
         {media.title}
       </p>
       <div className="flex w-full gap-4">
