4a7e9d4d832195379e815883e26292ab7a593e0d
rdx <reydux95@gmail.com>
2023-11-12 23:45:48 +0100

    chore(style): run ESLint

--- prisma/seeds/tags.ts
+++ prisma/seeds/tags.ts
@@ -1,4 +1,4 @@
-import { type PrismaClient } from "@prisma/client";
+import type { PrismaClient } from "@prisma/client";
 
 const execute = async (db: PrismaClient) => {
   await db.tag.createMany({
