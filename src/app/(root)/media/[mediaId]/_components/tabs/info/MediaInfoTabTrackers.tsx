bc74bb9a460916b7f0709184a401476a2bb4222f
rdx <reydux95@gmail.com>
2023-11-14 23:22:10 +0100

    feat(medias): use new Category component

--- src/app/(root)/media/[mediaId]/_components/tabs/info/MediaInfoTabTrackers.tsx
+++ src/app/(root)/media/[mediaId]/_components/tabs/info/MediaInfoTabTrackers.tsx
@@ -1,48 +1,33 @@
 import { Link } from "@nextui-org/link";
-import { tv } from "@nextui-org/react";
 
+import { Category } from "~/components/generics/Category";
 import { CompanyLogo } from "~/components/ui/CompanyLogo";
 import type { MediaLimited } from "~/lib/types";
 import { TrackerUtils } from "~/lib/utils/tracker.utils";
 
 type Props = { media: MediaLimited };
 
-const mediaInfoTabTrackers = tv({
-  slots: {
-    container: "items-start",
-    categoryWrapper: "flex flex-col gap-2 justify-start",
-    categoryTitle: "font-semibold text-lg",
-    categoryContent: "flex flex-wrap gap-2",
-  },
-});
-
-export const MediaInfoTabTrackers = ({ media }: Props) => {
-  const { container, categoryWrapper, categoryTitle, categoryContent } =
-    mediaInfoTabTrackers();
-
-  return (
-    <div className={container()}>
-      {media.trackers.length > 0 && (
-        <div className={categoryWrapper()}>
-          <h3 className={categoryTitle()}>Trackers</h3>
-          <div className={categoryContent()}>
-            {media.trackers.map((item) => (
-              <Link
-                key={item.tracker}
-                href={TrackerUtils.getTrackerUrl(item)}
-                className="flex select-none gap-1 rounded-md bg-default-200 px-2 py-1.5 text-sm text-foreground"
-              >
-                <CompanyLogo
-                  company={item.tracker}
-                  className="rounded-sm"
-                  height={16}
-                />
-                {TrackerUtils.getTrackerLabel(item.tracker)}
-              </Link>
-            ))}
-          </div>
+export const MediaInfoTabTrackers = ({ media }: Props) => (
+  <div className="items-start">
+    {media.trackers.length > 0 && (
+      <Category title="Trackers">
+        <div className="flex flex-wrap gap-2">
+          {media.trackers.map((item) => (
+            <Link
+              key={item.tracker}
+              href={TrackerUtils.getTrackerUrl(item)}
+              className="flex select-none gap-1 rounded-md bg-default-200 px-2 py-1.5 text-sm text-foreground"
+            >
+              <CompanyLogo
+                company={item.tracker}
+                className="rounded-sm"
+                height={16}
+              />
+              {TrackerUtils.getTrackerLabel(item.tracker)}
+            </Link>
+          ))}
         </div>
-      )}
-    </div>
-  );
-};
+      </Category>
+    )}
+  </div>
+);
