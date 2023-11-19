e602859fd631219239400dbe9a51048b1af2f026
rdx <reydux95@gmail.com>
2023-11-19 21:16:15 +0100

    fix(medias): title's shadow intensity

--- src/app/(root)/media/[mediaId]/_components/layout/MediaLayoutActions.tsx
+++ src/app/(root)/media/[mediaId]/_components/layout/MediaLayoutActions.tsx
@@ -10,7 +10,7 @@ export const MediaLayoutActions = ({ media }: Props) => {
   return (
     <div className="flex h-28 flex-col gap-2 py-3 xl:h-36">
       <div className="h-full">
-        <p className="line-clamp-1 h-[44px] text-center text-2xl font-bold [text-shadow:_0_3px_0_rgb(0_0_0_/_40%)] md:text-left md:text-4xl">
+        <p className="line-clamp-1 h-[44px] text-center text-2xl font-bold drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] md:text-left md:text-4xl">
           {media.mainTitle}
         </p>
       </div>
