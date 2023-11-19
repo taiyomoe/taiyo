f85429c31cb5c193a2db16c59069ca2326367fa9
rdx <reydux95@gmail.com>
2023-11-19 21:12:26 +0100

    feat(medias): translate genres

---
+++ src/lib/i18n/genres/genres.pt.ts
@@ -0,0 +1,23 @@
+import type { $Enums } from "@prisma/client";
+
+export const GENRES_PT: Record<$Enums.MediaGenres, string> = {
+  ACTION: "Ação",
+  ADVENTURE: "Aventura",
+  COMEDY: "Comédia",
+  DRAMA: "Drama",
+  ECCHI: "Ecchi",
+  FANTASY: "Fantasia",
+  HENTAI: "Hentai",
+  HORROR: "Horror",
+  MAHOU_SHOUJO: "Mahou Shoujo",
+  MECHA: "Mecha",
+  MUSIC: "Música",
+  MYSTERY: "Mistério",
+  PSYCHOLOGICAL: "Psicológico",
+  ROMANCE: "Romance",
+  SCI_FI: "Sci-fi",
+  SLICE_OF_LIFE: "Slice of Life",
+  SPORTS: "Esportes",
+  SUPERNATURAL: "Sobrenatural",
+  THRILLER: "Thriller",
+};
