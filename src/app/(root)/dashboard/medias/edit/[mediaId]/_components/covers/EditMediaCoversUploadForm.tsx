bcdb26e02453e1a3555675e83e66eb9e89d97c25
rdx <reydux95@gmail.com>
2023-12-07 02:06:27 +0100

    feat(covers): add upload dropzone

---
+++ src/app/(root)/dashboard/medias/edit/[mediaId]/_components/covers/EditMediaCoversUploadForm.tsx
@@ -0,0 +1,92 @@
+import { useCallback } from "react";
+import { ContentRating, Languages } from "@prisma/client";
+import { useFormikContext } from "formik";
+
+import { Form } from "~/components/generics/form/Form";
+import { InputFormField } from "~/components/generics/form/InputFormField";
+import { SelectFormField } from "~/components/generics/form/SelectFormField";
+import { SwitchFormField } from "~/components/generics/form/SwitchFormField";
+import { List } from "~/components/generics/List";
+import { NewImageDropzone } from "~/components/ui/images/NewImageDropzone";
+import type { UploadMediaCoverSchema } from "~/lib/schemas";
+
+export const EditMediaCoversUploadForm = () => {
+  const { setValues } = useFormikContext<UploadMediaCoverSchema>();
+
+  const onDrop = () => {
+    void setValues((prev) => [
+      ...prev,
+      {
+        contentRating: "NORMAL",
+        volume: prev.length + 1,
+        isMainCover: false,
+        language: "ja",
+      },
+    ]);
+  };
+
+  const handleSwitchChange = useCallback(
+    (imageIndex: number) => (newValue: boolean) => {
+      void setValues((prev) =>
+        prev.map((x, i) => ({
+          ...x,
+          isMainCover: newValue ? i === imageIndex : false,
+        })),
+      );
+    },
+    [setValues],
+  );
+
+  return (
+    <NewImageDropzone title="Upar covers" type="COVER" onDrop={onDrop}>
+      {({ selectedImages }) => (
+        <List className="gap-4">
+          {selectedImages.map(({ file }, i) => (
+            <div key={i} className="flex gap-2">
+              <img
+                className="h-[300px] w-[210px] rounded-medium object-cover"
+                src={URL.createObjectURL(file)}
+                alt={`Selected file ${i + 1}`}
+              />
+              <div className="grow p-2">
+                <Form.Col>
+                  <Form.Row>
+                    <SelectFormField
+                      label="Classificação"
+                      name={`[${i}].contentRating`}
+                      items={ContentRating}
+                      aria-label="Content rating"
+                      variant="faded"
+                    />
+                    <InputFormField
+                      label="Volume"
+                      labelPlacement="outside"
+                      name={`[${i}].volume`}
+                      type="number"
+                      variant="faded"
+                    />
+                  </Form.Row>
+                  <Form.Row>
+                    <SelectFormField
+                      label="Língua"
+                      name={`[${i}].language`}
+                      items={Languages}
+                      aria-label="Languages"
+                      variant="faded"
+                    />
+                    <SwitchFormField
+                      label="Capa principal"
+                      labelPlacement="outside"
+                      name={`[${i}].isMainCover`}
+                      onChange={handleSwitchChange(i)}
+                    />
+                  </Form.Row>
+                </Form.Col>
+              </div>
+            </div>
+          ))}
+        </List>
+      )}
+    </NewImageDropzone>
+  );
+};
