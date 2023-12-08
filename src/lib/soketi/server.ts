860e953e1757a66c9f8a3824cfee8af80a020713
rdx <reydux95@gmail.com>
2023-12-08 15:12:43 +0100

    fix(build): port error

--- src/lib/soketi/server.ts
+++ src/lib/soketi/server.ts
@@ -4,7 +4,7 @@ import { env } from "~/lib/env.mjs";
 
 export const pusherServer = new PusherServer({
   host: env.NEXT_PUBLIC_SOKETI_HOST,
-  port: env.NEXT_PUBLIC_SOKETI_PORT.toString(),
+  port: String(env.NEXT_PUBLIC_SOKETI_PORT),
   appId: env.SOKETI_APP_ID,
   key: env.NEXT_PUBLIC_SOKETI_APP_KEY,
   secret: env.SOKETI_APP_SECRET,
