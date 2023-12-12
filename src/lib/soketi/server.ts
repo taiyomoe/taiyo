fca824890b1f0bc2ca019259d4eb54782fd5d286
rdx <reydux95@gmail.com>
2023-12-12 21:07:44 +0100

    fix(soketi): use TLS on prod

--- src/lib/soketi/server.ts
+++ src/lib/soketi/server.ts
@@ -8,5 +8,5 @@ export const pusherServer = new PusherServer({
   appId: env.SOKETI_APP_ID,
   key: env.NEXT_PUBLIC_SOKETI_APP_KEY,
   secret: env.SOKETI_APP_SECRET,
-  useTLS: true,
+  useTLS: env.NODE_ENV === "production",
 });
