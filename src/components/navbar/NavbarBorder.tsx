d237af9fcf8a5a3a4b2343bac2b4568fe0ec2f8a
rdx <reydux95@gmail.com>
2023-12-21 14:02:37 +0100

    fix(navbar): don't use reader mode on normal pages

--- src/components/navbar/NavbarBorder.tsx
+++ src/components/navbar/NavbarBorder.tsx
@@ -3,18 +3,14 @@
 import { useBodyPadding } from "~/hooks/useBodyPadding";
 import { useScrollOpacity } from "~/hooks/useScrollOpacity";
 import { cn } from "~/lib/utils/cn";
-import { useReaderStore } from "~/stores";
 
 export const NavbarBorder = () => {
-  const { settings } = useReaderStore();
   const { opacity } = useScrollOpacity({ min: 0, max: 100 });
   useBodyPadding();
 
   return (
     <span
-      className={cn("relative h-[1px] bg-primary transition-all", {
-        ["-top-navbar group-hover:top-0"]: settings.navbarMode === "hover",
-      })}
+      className={cn("h-[1px] bg-primary transition-all")}
       style={{ opacity }}
     />
   );
