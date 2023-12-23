2bc5b55bb0dc17e2c69fc03cfd02d0b8f59bc346
Lusca Carvalho <71954875+Luscarvalho@users.noreply.github.com>
2023-12-23 18:30:10 -0400

    Update class names nigger

--- src/app/(root)/media/[mediaId]/_components/layout/MediaLayoutActions.tsx
+++ src/app/(root)/media/[mediaId]/_components/layout/MediaLayoutActions.tsx
@@ -13,7 +13,7 @@ type Props = {
 export const MediaLayoutActions = ({ media }: Props) => {
   return (
     <div className="flex h-28 flex-col justify-end gap-2 py-3 xl:h-36">
-      <p className="media-name line-clamp-1 pb-1 text-center text-2xl font-bold drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] md:text-left md:text-4xl xl:text-5xl">
+      <p className="media-title line-clamp-1 pb-1 text-center text-2xl font-bold drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] md:text-left md:text-4xl xl:text-5xl">
         {media.mainTitle}
       </p>
       <div className="flex gap-4">
