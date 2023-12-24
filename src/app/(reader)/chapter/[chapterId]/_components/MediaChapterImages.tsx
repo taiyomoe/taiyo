4afee2483595d3311b9b89f530b6c6a58b642623
rdx <reydux95@gmail.com>
2023-12-24 17:55:49 +0100

    fix(reader): rework image preloading

--- src/app/(reader)/chapter/[chapterId]/_components/MediaChapterImages.tsx
+++ src/app/(reader)/chapter/[chapterId]/_components/MediaChapterImages.tsx
@@ -6,16 +6,22 @@ import { useReaderStore } from "~/stores";
 import { MediaChapterImage } from "./MediaChapterImage";
 
 export const MediaChapterImages = () => {
-  const { settings, currentPageNumber, images } = useReaderStore();
+  const { settings, currentPageNumber, getImages } = useReaderStore();
+  const images = getImages();
+  const currentImage = images.find((img) => img.number === currentPageNumber);
+
   useChapterProgression();
 
-  const currentImage = images.find((img) => img.number === currentPageNumber);
+  if (images.length === 0) {
+    return (
+      <div className="flex h-full items-center justify-center">
+        <Spinner size="lg" className="m-4 justify-self-center" />
+      </div>
+    );
+  }
 
   return (
     <div className="mx-auto flex h-full flex-col items-center justify-center">
-      {(images.length === 0 || !currentImage) && (
-        <Spinner size="lg" className="m-4 justify-self-center" />
-      )}
       {images
         .sort((a, b) => a.number - b.number)
         .map((img) => (
