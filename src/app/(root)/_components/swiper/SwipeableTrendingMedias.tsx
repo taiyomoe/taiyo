2d7a167dce98d0c50654d3e78565058a499774bf
rdx <reydux95@gmail.com>
2023-12-26 10:28:47 +0100

    feat(home): replace trending media's swiper with embla

--- src/app/(root)/_components/swiper/SwipeableTrendingMedias.tsx
+++ src/app/(root)/_components/swiper/SwipeableTrendingMedias.tsx
@@ -1,64 +1,63 @@
 "use client"
 
-import { Image } from "@nextui-org/image"
-import { FreeMode, Mousewheel, Pagination } from "swiper/modules"
-import { Swiper, SwiperSlide } from "swiper/react"
-
-import "swiper/css"
-import "swiper/css/pagination"
-
-import NextImage from "next/image"
+import useEmblaCarousel from "embla-carousel-react"
 import Link from "next/link"
 
+import { MediaImage } from "~/components/generics/images/MediaImage"
+import { useDevice } from "~/hooks/useDevice"
 import type { LatestMedia } from "~/lib/types"
+import { cn } from "~/lib/utils/cn"
 import { MediaCoverUtils } from "~/lib/utils/mediaCover.utils"
 
 type Props = {
   medias: LatestMedia[]
 }
 
-export const SwipeableTrendingMedias = ({ medias }: Props) => (
-  <Swiper
-    direction="horizontal"
-    slidesPerView="auto"
-    className="trendingMedias h-[400px] w-full md:h-[498px] md:w-[350px]"
-    spaceBetween={24}
-    freeMode
-    breakpoints={{
+export const SwipeableTrendingMedias = ({ medias }: Props) => {
+  const { isAboveTablet } = useDevice()
+  const [emblaRef] = useEmblaCarousel({
+    dragFree: true,
+    axis: "x",
+    loop: false,
+    breakpoints: {
       // MD and up
-      768: {
-        direction: "vertical",
-        freeMode: false,
-        slidesPerView: 1,
+      "(min-width: 768px)": {
+        dragFree: false,
+        axis: "y",
+        loop: true,
       },
-    }}
-    pagination={{
-      clickable: true,
-      dynamicBullets: true,
-    }}
-    mousewheel={{
-      sensitivity: 0.2,
-    }}
-    modules={[Pagination, FreeMode, Mousewheel]}
-  >
-    {medias.map((media) => (
-      <SwiperSlide key={media.id}>
-        <Link
-          href={`/media/${media.id}`}
-          className="relative hover:cursor-pointer"
-          passHref
-        >
-          <Image
-            as={NextImage}
-            src={MediaCoverUtils.getUrl(media)}
-            className="max-h-[400px] min-h-[400px] w-[280px] object-cover md:max-h-[498px] md:min-h-[498px] md:w-[350px]"
-            width={350}
-            height={0}
-            alt="media's cover"
-            isZoomed
-          />
-        </Link>
-      </SwiperSlide>
-    ))}
-  </Swiper>
-)
+    },
+  })
+
+  return (
+    <div className="overflow-hidden" ref={emblaRef}>
+      <div className="flex flex-col max-h-[498px]">
+        {medias.map((media, i) => (
+          <Link
+            key={media.id}
+            href={`/media/${media.id}`}
+            className={cn(
+              "relative hover:cursor-pointer min-h-[400px] max-h-[400px] md:min-h-[498px] md:max-h-[498px]",
+              {
+                "mr-6": !isAboveTablet && i !== medias.length - 1,
+                "mb-6": isAboveTablet && i !== medias.length - 1,
+              },
+            )}
+          >
+            <MediaImage
+              src={MediaCoverUtils.getUrl(media)}
+              classNames={{
+                height: "h-[400px] md:h-[498px]",
+                width: "w-[280px] md:w-[350px]",
+              }}
+              maxHeight={498}
+              maxWidth={350}
+              isZoomed
+              alt="media's cover"
+            />
+          </Link>
+        ))}
+      </div>
+    </div>
+  )
+}
