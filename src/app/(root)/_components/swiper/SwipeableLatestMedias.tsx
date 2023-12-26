5dae3d1d650dda76e1cb114bced485d190d57c69
rdx <reydux95@gmail.com>
2023-12-26 10:02:08 +0100

    feat(home): replace latest media's swiper with embla

--- src/app/(root)/_components/swiper/SwipeableLatestMedias.tsx
+++ src/app/(root)/_components/swiper/SwipeableLatestMedias.tsx
@@ -1,52 +1,52 @@
 "use client"
 
 import { Image } from "@nextui-org/image"
-import { FreeMode, Mousewheel } from "swiper/modules"
-import { Swiper, SwiperSlide } from "swiper/react"
-
-import "swiper/css"
+import useEmblaCarousel from "embla-carousel-react"
 
 import NextImage from "next/image"
 import Link from "next/link"
+import { MediaImage } from "~/components/generics/images/MediaImage"
 
 import type { LatestMedia } from "~/lib/types"
+import { cn } from "~/lib/utils/cn"
 import { MediaCoverUtils } from "~/lib/utils/mediaCover.utils"
 
 type Props = {
   medias: LatestMedia[]
 }
 
-export const SwipeableLatestMedias = ({ medias }: Props) => (
-  <Swiper
-    className="latestMedias h-[300px]"
-    slidesPerView="auto"
-    spaceBetween={24}
-    modules={[FreeMode, Mousewheel]}
-    mousewheel={{
-      sensitivity: 0.5,
-    }}
-    freeMode={{
-      enabled: false,
-    }}
-  >
-    {medias.map((media) => (
-      <SwiperSlide key={media.id} className="!w-fit md:!w-auto">
-        <Link
-          href={`/media/${media.id}`}
-          className="relative hover:cursor-pointer"
-          passHref
-        >
-          <Image
-            as={NextImage}
-            src={MediaCoverUtils.getUrl(media)}
-            className="max-h-[300px] min-h-[300px] object-cover"
-            height={300}
-            width={210}
-            alt="media's cover"
-            isZoomed
-          />
-        </Link>
-      </SwiperSlide>
-    ))}
-  </Swiper>
-)
+export const SwipeableLatestMedias = ({ medias }: Props) => {
+  const [emblaRef] = useEmblaCarousel({ dragFree: true })
+
+  return (
+    <div className="overflow-hidden" ref={emblaRef}>
+      <div className="flex">
+        {medias.map((media, i) => (
+          <Link
+            key={media.id}
+            href={`/media/${media.id}`}
+            className={cn(
+              "relative hover:cursor-pointer w-fit min-h-[300px] max-h-[300px]",
+              {
+                "mr-6": i !== medias.length - 1,
+              },
+            )}
+          >
+            <MediaImage
+              src={MediaCoverUtils.getUrl(media)}
+              classNames={{
+                wrapper: "h-full",
+                height: "h-full",
+                width: "min-w-[210px]",
+              }}
+              maxHeight={300}
+              maxWidth={210}
+              isZoomed
+              alt="media's cover"
+            />
+          </Link>
+        ))}
+      </div>
+    </div>
+  )
+}
