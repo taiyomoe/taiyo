3e7d9a4b273de9bf0637b46c18e472fd4c54f51f
rdx <reydux95@gmail.com>
2023-12-26 01:12:54 +0100

    fix(reader): overlay issue with different navbar modes

--- src/app/(reader)/_components/MediaChapterPageOverlay.tsx
+++ src/app/(reader)/_components/MediaChapterPageOverlay.tsx
@@ -1,17 +1,79 @@
+import { useCallback, useEffect, useState } from "react";
 import Link from "next/link";
+import { tv } from "@nextui-org/react";
 
 import { MediaChapterPageOverlayScrollButton } from "~/app/(reader)/_components/readerSidebar/MediaChapterPageOverlayScrollButton";
 import { ReaderSidebarOpenButton } from "~/components/navbar/ReaderSidebarOpenButton";
 import { useDevice } from "~/hooks/useDevice";
+import { cn } from "~/lib/utils/cn";
 import { MediaUtils } from "~/lib/utils/media.utils";
 import { useReaderSettingsStore, useReaderStore } from "~/stores";
 
 import { ReaderSettingsMediaChapterDropdown } from "./readerSidebar/ui/ReaderSettingsMediaChapterDropdown";
 
+const mediaChapterPageOverlay = tv({
+  slots: {
+    container:
+      "fixed z-10 w-full px-bodyPadding bg-black bg-opacity-60 transition-all invisible data-[show=true]:visible",
+    topContainer:
+      "top-0 -mt-[100px] h-[100px] space-y-4 py-2 data-[show=true]:mt-0",
+    bottomContainer:
+      "bottom-0 -mb-[100px] flex justify-end gap-4 py-3 data-[show=true]:mb-0",
+    title:
+      "line-clamp-1 text-center text-2xl font-semibold drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] hover:underline",
+  },
+  variants: {
+    navbarMode: {
+      fixed: {},
+      sticky: {
+        topContainer: "top-navbar data-[show=true]:visible",
+      },
+      hover: {},
+    },
+  },
+});
+
 export const MediaChapterPageOverlay = () => {
-  const { page } = useReaderSettingsStore();
+  const { page, navbarMode } = useReaderSettingsStore();
   const { chapter } = useReaderStore();
   const { isAboveTablet } = useDevice();
+  const [topContainerTop, setTopContainerTop] = useState<number | undefined>(
+    navbarMode === "fixed" ? 60 : undefined,
+  );
+
+  const slots = mediaChapterPageOverlay({ navbarMode });
+
+  const computeTopContainerTop = useCallback(
+    (yPosition: number) => {
+      if (yPosition <= 60) {
+        setTopContainerTop(60 - yPosition);
+      } else if (topContainerTop !== undefined) {
+        setTopContainerTop(undefined);
+      }
+    },
+    [topContainerTop],
+  );
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (navbarMode !== "fixed") {
+        return;
+      }
+
+      const scrollY = window.scrollY;
+      computeTopContainerTop(scrollY);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  });
+
+  useEffect(() => {
+    computeTopContainerTop(window.scrollY);
+  }, [computeTopContainerTop, navbarMode]);
 
   if (page.mode === "single" || isAboveTablet || !chapter) {
     return null;
@@ -20,19 +82,17 @@ export const MediaChapterPageOverlay = () => {
   return (
     <>
       <div
-        className="fixed top-0 z-10 -mt-[100px] h-[100px] w-full space-y-4 bg-black bg-opacity-60 px-bodyPadding py-2 transition-all data-[show=true]:mt-0"
+        className={cn(slots.container(), slots.topContainer())}
         data-show={page.overlay === "show"}
+        style={{ top: topContainerTop }}
       >
-        <Link
-          href={MediaUtils.getUrl(chapter.media)}
-          className="line-clamp-1 text-center text-2xl font-semibold drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] hover:underline"
-        >
+        <Link href={MediaUtils.getUrl(chapter.media)} className={slots.title()}>
           {chapter.media.title}
         </Link>
         <ReaderSettingsMediaChapterDropdown />
       </div>
       <div
-        className="fixed bottom-0 z-10 -mb-[100px] flex w-full justify-end gap-4 bg-black bg-opacity-60 px-bodyPadding py-3 transition-all data-[show=true]:mb-0"
+        className={cn(slots.container(), slots.bottomContainer())}
         data-show={page.overlay === "show"}
       >
         <MediaChapterPageOverlayScrollButton />
