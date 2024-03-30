17ab6c591c46a91e0729cad1014720547ee197c2
rdx <reydux95@gmail.com>
2024-03-30 11:56:00 +0100

    fix(titles): rename type

--- apps/taiyo/src/stores/mediaUpdate.store.ts
+++ apps/taiyo/src/stores/mediaUpdate.store.ts
@@ -1,7 +1,7 @@
 import type { MediaCover, MediaTitle } from "@prisma/client"
 import type {
   UpdateMediaCoverSchema,
-  UpdateMediaTitleSchema,
+  UpdateMediaTitleInput,
 } from "@taiyomoe/schemas"
 import type { MediaWithRelations } from "@taiyomoe/types"
 import { create } from "zustand"
@@ -13,7 +13,7 @@ export type MediaUpdateState = {
   load: (media: MediaWithRelations) => void
 
   addTitle: (payload: MediaTitle) => void
-  updateTitle: (payload: UpdateMediaTitleSchema) => void
+  updateTitle: (payload: UpdateMediaTitleInput) => void
 
   addCover: (payload: MediaCover[]) => void
   updateCover: (payload: UpdateMediaCoverSchema) => void
