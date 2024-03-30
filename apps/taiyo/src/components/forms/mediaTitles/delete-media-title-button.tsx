d197d45a99334eadd284905f8ee5b3c89364e0bd
rdx <reydux95@gmail.com>
2024-03-30 11:31:35 +0100

    refactor(titles): rework delete button

--- apps/taiyo/src/components/forms/mediaTitles/delete-media-title-button.tsx
+++ apps/taiyo/src/components/forms/mediaTitles/delete-media-title-button.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@nextui-org/react"
-import type { UpdateMediaCoverSchema } from "@taiyomoe/schemas"
-import { useFormikContext } from "formik"
+import type { UpdateMediaTitleInput } from "@taiyomoe/schemas"
+import { useFormContext } from "react-hook-form"
 import { toast } from "sonner"
 import { useMediaUpdateStore } from "~/stores"
 import { api } from "~/trpc/react"
@@ -10,15 +10,20 @@ type Props = {
 }
 
 export const DeleteMediaTitleButton = ({ toggleModal }: Props) => {
-  const { initialValues, values } = useFormikContext<UpdateMediaCoverSchema>()
+  const {
+    getValues,
+    formState: { defaultValues },
+  } = useFormContext<UpdateMediaTitleInput>()
   const { del } = useMediaUpdateStore()
   const { mutateAsync } = api.mediaTitles.delete.useMutation()
 
   const handlePress = () => {
-    toast.promise(mutateAsync({ id: values.id }), {
+    const id = getValues("id")
+
+    toast.promise(mutateAsync({ id }), {
       loading: "Apagando o título...",
       success: () => {
-        del("title", values.id)
+        del("title", id)
         toggleModal()
 
         return "Título apagado com sucesso!"
@@ -30,7 +35,7 @@ export const DeleteMediaTitleButton = ({ toggleModal }: Props) => {
   return (
     <Button
       onPress={handlePress}
-      isDisabled={initialValues.isMainCover}
+      isDisabled={defaultValues?.isMainTitle}
       variant="light"
       color="danger"
     >
