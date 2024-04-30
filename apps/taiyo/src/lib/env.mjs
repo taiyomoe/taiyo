daea5e14bf5bbbd6c5ae5d9a2ab4e757794df086
rdx <reydux95@gmail.com>
2024-04-30 13:52:28 +0200

    chore(io): remove unused env variables

--- apps/taiyo/src/lib/env.mjs
+++ apps/taiyo/src/lib/env.mjs
@@ -90,10 +90,6 @@ export const env = createEnv({
     DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID,
     DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET,
 
-    // Encryption
-    ENCRYPTION_KEY: process.env.ENCRYPTION_KEY,
-    ENCRYPTION_IV: process.env.ENCRYPTION_IV,
-
     // CDN
     NEXT_PUBLIC_CDN_URL: process.env.NEXT_PUBLIC_CDN_URL,
 
