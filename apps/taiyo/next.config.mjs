517cff188f5143b623b8d1ac323b1a0c3b19c0ee
rdx <reydux95@gmail.com>
2024-05-07 23:20:39 +0200

    ci: ignore TS & ESLint during build

--- apps/taiyo/next.config.mjs
+++ apps/taiyo/next.config.mjs
@@ -44,8 +44,8 @@ const config = {
   ],
 
   /** We already do linting and typechecking as separate tasks in CI */
-  // eslint: { ignoreDuringBuilds: true },
-  // typescript: { ignoreBuildErrors: true },
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
 }
 
 export default config
