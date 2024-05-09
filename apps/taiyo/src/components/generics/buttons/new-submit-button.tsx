49ada447fc364d8d05de0766a039ea366ec6a7b2
rdx <reydux95@gmail.com>
2024-05-09 22:57:42 +0200

    chore: remove console.logs

--- apps/taiyo/src/components/generics/buttons/new-submit-button.tsx
+++ apps/taiyo/src/components/generics/buttons/new-submit-button.tsx
@@ -10,13 +10,6 @@ export const SubmitButton = ({ isDisabled, children }: ButtonProps) => {
     isDisabled ||
     Object.keys(errors).length !== 0
 
-  // console.log("isSubmitting", isSubmitting)
-  // console.log("isValid", isValid)
-  // console.log("isDirty", isDirty)
-  // console.log("isDisabled", isDisabled)
-  // console.log("values", getValues())
-  // console.log("errors", errors)
-
   return (
     <Button
       className="w-fit font-medium"
--- apps/taiyo/src/stores/readerSettings.store.ts
+++ apps/taiyo/src/stores/readerSettings.store.ts
@@ -38,8 +38,6 @@ export const useReaderSettingsStore = create<ReaderSettings & Actions>(
       set((state) => {
         const newSettings = structuredClone(_.omit(state, "update", "reset"))
 
-        console.log("Updating", key, "to", newValue)
-
         _.set(newSettings, key, newValue)
 
         // If the user is trying to open the sidebar while the page overlay is open, close the overlay
@@ -61,8 +59,6 @@ export const useReaderSettingsStore = create<ReaderSettings & Actions>(
         }
 
         if (persistent) {
-          console.log("Updating persistent settings", key, "to", newValue)
-
           usePersistentReaderSettingsStore.getState().update(key, newValue)
         }
 
