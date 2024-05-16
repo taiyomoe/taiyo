6fbb3e0d9629ca2c1ae92e0b14716387cc638412
rdx <reydux95@gmail.com>
2024-05-16 14:18:29 +0200

    chore(forms): uninstall formik & formik-related components and dependencies

--- apps/taiyo/src/lib/types/form.types.ts
+++ apps/taiyo/src/lib/types/form.types.ts
@@ -1,7 +1,3 @@
-import type { FormikConfig, FormikValues } from "formik"
-
-export type FormSubmit<T extends FormikValues> = FormikConfig<T>["onSubmit"]
-
 export type SelectItem = { label: string; value: string }
 
 export type RangeValue = string
