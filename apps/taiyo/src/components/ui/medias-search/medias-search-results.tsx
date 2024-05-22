04263a42a04daa2aa5c70fc6979f395fc0aae1b6
rdx <reydux95@gmail.com>
2024-05-23 00:30:58 +0200

    feat(medias): add search items

--- apps/taiyo/src/components/ui/medias-search/medias-search-results.tsx
+++ apps/taiyo/src/components/ui/medias-search/medias-search-results.tsx
@@ -1,13 +1,15 @@
 import { ScrollShadow } from "@nextui-org/react"
+import { useSession } from "@taiyomoe/auth/client"
 import type { MediasIndexItem } from "@taiyomoe/types"
 import { Command } from "cmdk"
+import { MediasSearchResultsItem } from "./medias-search-results-item"
 
 type Props = {
   items: MediasIndexItem[]
 }
 
 export const MediasSearchResults = ({ items }: Props) => {
-  console.log("items", items)
+  const { data: session } = useSession()
 
   return (
     <Command.Group
@@ -18,12 +20,14 @@ export const MediasSearchResults = ({ items }: Props) => {
       forceMount
     >
       {items.length === 0 && <Command.Empty>No results found.</Command.Empty>}
-      <Command.List className="flex grow bg-emerald-600">
-        <ScrollShadow className="scrollbar-none h-[300px] w-full">
+      <Command.List>
+        <ScrollShadow className="scrollbar-none mt-2 flex h-[300px] w-full grow flex-col gap-1.5">
           {items.map((item) => (
-            <Command.Item key={item.id}>
-              {item.titles.at(0)?.title}
-            </Command.Item>
+            <MediasSearchResultsItem
+              key={item.id}
+              item={item}
+              preferredTitles={session?.user.preferredTitles}
+            />
           ))}
         </ScrollShadow>
       </Command.List>
