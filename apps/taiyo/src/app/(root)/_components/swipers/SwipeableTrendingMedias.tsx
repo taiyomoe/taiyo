00f1ef1f5e032bd7f784f3a0ff217c721c187f1a
rdx <reydux95@gmail.com>
2024-05-23 10:35:53 +0200

    fix(home): hydration error with trending medias

--- apps/taiyo/src/app/(root)/_components/swipers/SwipeableTrendingMedias.tsx
+++ apps/taiyo/src/app/(root)/_components/swipers/SwipeableTrendingMedias.tsx
@@ -5,15 +5,12 @@ import { MediaCoverUtils } from "@taiyomoe/utils"
 import useEmblaCarousel from "embla-carousel-react"
 import Link from "next/link"
 import { MediaImage } from "~/components/generics/images/MediaImage"
-import { useDevice } from "~/hooks/useDevice"
-import { cn } from "~/lib/utils/cn"
 
 type Props = {
   medias: LatestMedia[]
 }
 
 export const SwipeableTrendingMedias = ({ medias }: Props) => {
-  const device = useDevice()
   const [emblaRef] = useEmblaCarousel({
     dragFree: true,
     axis: "x",
@@ -31,17 +28,11 @@ export const SwipeableTrendingMedias = ({ medias }: Props) => {
   return (
     <div className="overflow-hidden" ref={emblaRef}>
       <div className="flex max-h-[400px] flex-row md:max-h-[498px] md:flex-col">
-        {medias.map((media, i) => (
+        {medias.map((media) => (
           <Link
             key={media.id}
             href={`/media/${media.id}`}
-            className={cn(
-              "relative max-h-[400px] min-h-[400px] md:max-h-[498px] md:min-h-[498px] hover:cursor-pointer",
-              {
-                "mr-6": !device?.isAboveTablet && i !== medias.length - 1,
-                "mb-6": device?.isAboveTablet && i !== medias.length - 1,
-              },
-            )}
+            className="relative mr-6 max-h-[400px] min-h-[400px] last:mr-0 lg:mr-0 lg:mb-6 md:max-h-[498px] md:min-h-[498px] hover:cursor-pointer"
           >
             <MediaImage
               src={MediaCoverUtils.getUrl(media)}
