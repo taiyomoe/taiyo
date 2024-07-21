6021d0ec8911230d1a90e46f60e30a898b8feeb6
rdx <reydux95@gmail.com>
2024-07-21 17:32:06 +0200

    chore(env): add missing env vars

--- apps/image-orchestrator/src/env.ts
+++ apps/image-orchestrator/src/env.ts
@@ -1,12 +1,13 @@
 import { createEnv } from "@t3-oss/env-core"
 import { vercel } from "@t3-oss/env-core/presets"
+import { env as authEnv } from "@taiyomoe/auth/env"
 import { env as cacheEnv } from "@taiyomoe/cache/env"
 import { env as meilisearchEnv } from "@taiyomoe/meilisearch/env"
 import { env as s3Env } from "@taiyomoe/s3/env"
 import { env as utilsEnv } from "@taiyomoe/utils/env"
 
 export const env = createEnv({
-  extends: [cacheEnv, meilisearchEnv, s3Env, utilsEnv, vercel()],
+  extends: [authEnv, cacheEnv, meilisearchEnv, s3Env, utilsEnv, vercel()],
 
   /**
    * Specify your shared environment variables schema here.
--- packages/s3/env.ts
+++ packages/s3/env.ts
@@ -15,6 +15,7 @@ export const env = createEnv({
     S3_URL: z.string().url(),
     S3_ACCESS_KEY_ID: z.string(),
     S3_SECRET_ACCESS_KEY: z.string(),
+    S3_BUCKET_NAME: z.string(),
   },
 
   /**
