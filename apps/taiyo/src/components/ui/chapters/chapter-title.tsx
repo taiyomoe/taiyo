4a4d7fdf4dc81b772212e317342dffc06cb6ef88
rdx <reydux95@gmail.com>
2024-08-16 16:19:46 +0200

    fix(medias): add chapters card title missing space

--- apps/taiyo/src/components/ui/chapters/chapter-title.tsx
+++ apps/taiyo/src/components/ui/chapters/chapter-title.tsx
@@ -5,6 +5,6 @@ type Props = {
 export const ChapterTitle = ({ chapter }: Props) => (
   <p className="line-clamp-1">
     Capítulo <span className="font-medium">{chapter.number}</span>
-    {chapter.title && `— ${chapter.title}`}
+    {chapter.title && ` — ${chapter.title}`}
   </p>
 )
