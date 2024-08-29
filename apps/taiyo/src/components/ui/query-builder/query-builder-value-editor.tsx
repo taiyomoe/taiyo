3e16f648cf9c95c075636afd11c6500747ed8fa3
rdx <reydux95@gmail.com>
2024-08-29 22:57:13 +0200

    feat(chapters): add scans filter

--- apps/taiyo/src/components/ui/query-builder/query-builder-value-editor.tsx
+++ apps/taiyo/src/components/ui/query-builder/query-builder-value-editor.tsx
@@ -1,5 +1,6 @@
 import { Input } from "@nextui-org/react"
 import { ValueEditor, type ValueEditorProps } from "react-querybuilder"
+import { ScansMultiAutocomplete } from "~/components/ui/multi-autocompletes/scans-multi-autocomplete"
 
 export const QueryBuilderValueEditor = (props: ValueEditorProps) => {
   // if (props.fieldData.datatype === "date") {
@@ -18,7 +19,18 @@ export const QueryBuilderValueEditor = (props: ValueEditorProps) => {
   //   )
   // }
   if (props.fieldData.datatype === "number") {
-    return <Input onValueChange={props.handleOnChange} />
+    return (
+      <Input className="min-w-[300px]" onValueChange={props.handleOnChange} />
+    )
+  }
+
+  if (props.fieldData.datatype === "scan") {
+    return (
+      <ScansMultiAutocomplete
+        classNames={{ container: () => "min-w-[300px]" }}
+        onChange={(values) => props.handleOnChange(values.map((s) => s.value))}
+      />
+    )
   }
 
   console.log("value editor props", props)
