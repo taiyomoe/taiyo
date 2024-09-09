1ac0ec53df41cf4299aba330b1259ac06282660b
rdx <reydux95@gmail.com>
2024-09-09 04:40:06 +0200

    chore(deps): bump @react-aria/interactions

--- apps/taiyo/src/components/ui/upload/folder-selection.tsx
+++ apps/taiyo/src/components/ui/upload/folder-selection.tsx
@@ -16,11 +16,11 @@ export const FolderSelection = forwardRef<HTMLButtonElement, Props>(
   ({ className, isSelected, ...props }, ref) => (
     <FileTrigger acceptDirectory {...props}>
       <Button
+        ref={ref}
         className={cn(
           "rounded-medium bg-content1 shadow-medium outline-none transition-[background,transform] data-[pressed=true]:scale-[0.97] data-[hovered=true]:bg-content2",
           className,
         )}
-        ref={ref}
         data-selected={isSelected}
       >
         <AssetSelection type="folder" />
