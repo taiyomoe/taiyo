d2f131b06fd490448ad33a776c844b91e0067e6d
rdx <reydux95@gmail.com>
2024-09-17 03:06:47 +0200

    feat(i18n): add base translation mechanism

--- apps/taiyo/src/app/(root)/dashboard/page.tsx
+++ apps/taiyo/src/app/(root)/dashboard/page.tsx
@@ -1,7 +1,11 @@
+import { useTranslations } from "next-intl"
+
 export default function Page() {
+  const t = useTranslations("HomePage")
+
   return (
     <div className="p-4">
-      <p className="text-foreground">Azerty</p>
+      <p className="text-foreground">{t("title")}</p>
     </div>
   )
 }
---
+++ apps/taiyo/src/i18n/request.ts
@@ -0,0 +1,13 @@
+import { getRequestConfig } from "next-intl/server"
+
+export default getRequestConfig(async () => {
+  // Provide a static locale, fetch a user setting,
+  // read from `cookies()`, `headers()`, etc.
+  const locale = "en"
+
+  return {
+    locale,
+    messages: (await import(`../../../../packages/messages/src/${locale}.json`))
+      .default,
+  }
+})
