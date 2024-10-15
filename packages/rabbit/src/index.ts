b63dc98c291b35bf5914b0cb6a746bb901c08acc
rdx <reydux95@gmail.com>
2024-10-15 22:03:57 +0200

    feat(rabbit): add client

---
+++ packages/rabbit/src/index.ts
@@ -0,0 +1,10 @@
+import Connection from "rabbitmq-client"
+import { env } from "../env"
+
+const rawRabbitClient = new Connection(env.RABBITMQ_URL)
+
+export const rabbitPublisher = rawRabbitClient.createPublisher({
+  confirm: true,
+  maxAttempts: 3,
+  exchanges: [{ exchange: "chapters", type: "topic" }],
+})
