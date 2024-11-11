a4961c94561a0d0e6f36478a8c4e9213d02ce309
rdx <reydux95@gmail.com>
2024-11-11 23:44:03 +0100

    fix(io-v4): cors issue

--- apps/io-v4/src/index.ts
+++ apps/io-v4/src/index.ts
@@ -3,12 +3,13 @@ import { Hono } from "hono"
 import { cors } from "hono/cors"
 import { chaptersController } from "~/controllers/chapters.controller"
 import { mediasController } from "~/controllers/medias.controller"
+import { env } from "~/env"
 import { withHelpers } from "~/middlewares/helpers.middleware"
 import { formatError } from "~/utils/format-error"
 import { HttpError } from "~/utils/http-error"
 
 const app = new Hono()
-  .use("*", cors({ origin: "http://localhost:3000", credentials: true }))
+  .use("*", cors({ origin: env.AUTH_URL, credentials: true }))
   .use(withHelpers)
   .route("/v4/medias", mediasController)
   .route("/v4/chapters", chaptersController)
