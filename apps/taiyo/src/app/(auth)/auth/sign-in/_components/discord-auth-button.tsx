97a0e768cecfa1f27c1f4f04f02e58ceb972efa8
rdx <reydux95@gmail.com>
2024-11-11 23:28:30 +0100

    refactor(auth): remove auth card in favor of a simple discord button

--- apps/taiyo/src/app/(auth)/auth/sign-in/_components/discord-auth-button.tsx
+++ apps/taiyo/src/app/(auth)/auth/sign-in/_components/discord-auth-button.tsx
@@ -5,16 +5,17 @@ import { CompanyLogo } from "~/components/ui/CompanyLogo"
 export const DiscordAuthButton = () => (
   <form>
     <Button
-      className="w-full bg-discord font-medium text-discord-foreground text-medium hover:bg-discord/80"
+      className="w-full bg-discord font-medium text-discord-foreground hover:bg-discord/80"
       startContent={<CompanyLogo company="discord" height={28} />}
       formAction={async () => {
         "use server"
         await signIn("discord")
       }}
       radius="full"
+      size="lg"
       type="submit"
     >
-      <p className="w-full">Discord</p>
+      Discord
     </Button>
   </form>
 )
--- apps/taiyo/src/app/(auth)/auth/sign-in/page.tsx
+++ apps/taiyo/src/app/(auth)/auth/sign-in/page.tsx
@@ -1,8 +1,8 @@
 import { auth } from "@taiyomoe/auth"
 import Link from "next/link"
 import { redirect } from "next/navigation"
+import { DiscordAuthButton } from "~/app/(auth)/auth/sign-in/_components/discord-auth-button"
 import { CompanyLogo } from "~/components/ui/CompanyLogo"
-import { AuthCard } from "./_components/AuthCard"
 
 export default async function SignInPage() {
   const session = await auth()
@@ -12,12 +12,19 @@ export default async function SignInPage() {
   }
 
   return (
-    <div className="flex w-full flex-col items-center gap-8 px-8">
-      <Link href="/" className="flex flex-col items-center gap-8 sm:flex-row">
+    <div className="flex w-full flex-col items-center gap-12 px-8">
+      <Link
+        href="/"
+        className="flex flex-col items-center gap-8 transition-transform hover:scale-[1.02] sm:flex-row"
+      >
         <CompanyLogo company="taiyo" width={150} height={100} />
         <p className="font-bold text-5xl">Taiyō</p>
       </Link>
-      <AuthCard />
+      <div className="text-center">
+        <p>Por enquanto, a única opção de login é o Discord.</p>
+        <p className="mb-4">Mais opções aparecerão em breve.</p>
+        <DiscordAuthButton />
+      </div>
     </div>
   )
 }
