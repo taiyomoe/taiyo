46d6db83dc6be6fc9fb9123ab50e16de120baab1
rdx <reydux95@gmail.com>
2024-11-12 03:58:54 +0100

    refactor: remove coolify_url

--- apps/taiyo/src/trpc/shared.ts
+++ apps/taiyo/src/trpc/shared.ts
@@ -2,6 +2,5 @@ import { env } from "~/env"
 
 export const getBaseUrl = () => {
   if (typeof window !== "undefined") return window.location.origin
-  if (process.env.COOLIFY_URL) return process.env.COOLIFY_URL
   return env.AUTH_URL
 }
