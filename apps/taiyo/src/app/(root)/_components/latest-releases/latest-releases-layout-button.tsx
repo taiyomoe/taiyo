90f79e461ac1fdba830091c459bf0dc1ba589b15
rdx <reydux95@gmail.com>
2024-11-14 02:16:39 +0100

    feat(home): save settings when selecting new layout

--- apps/taiyo/src/app/(root)/_components/latest-releases/latest-releases-layout-button.tsx
+++ apps/taiyo/src/app/(root)/_components/latest-releases/latest-releases-layout-button.tsx
@@ -1,12 +1,14 @@
 "use client"
 
 import { Button, ButtonGroup } from "@nextui-org/button"
+import { useSession } from "@taiyomoe/auth/client"
 import type { HomeLayout } from "@taiyomoe/db"
 import { useAtom } from "jotai"
 import { useHydrateAtoms } from "jotai/utils"
 import { Columns2Icon, Rows2Icon } from "lucide-react"
 import { releasesLayoutAtom } from "~/atoms/homeLayout.atoms"
 import { siteConfig } from "~/lib/config"
+import { api } from "~/trpc/react"
 
 type Props = {
   initialLayout: HomeLayout
@@ -16,8 +18,10 @@ export const LatestReleasesLayoutButton = ({ initialLayout }: Props) => {
   useHydrateAtoms([[releasesLayoutAtom, initialLayout]])
 
   const [releasesLayout, setReleasesLayout] = useAtom(releasesLayoutAtom)
+  const { data: session } = useSession()
+  const { mutateAsync } = api.users.updateSettings.useMutation()
 
-  const handlePress = (action: "ROWS" | "COLUMNS") => () => {
+  const handlePress = (action: "ROWS" | "COLUMNS") => async () => {
     const releasesLayoutContainer = document.getElementById(
       siteConfig.home.releasesLayoutContainerId,
     )
@@ -26,6 +30,10 @@ export const LatestReleasesLayoutButton = ({ initialLayout }: Props) => {
 
     setReleasesLayout(action)
     releasesLayoutContainer.setAttribute("data-releases-layout", action)
+
+    if (session) {
+      await mutateAsync({ homeLayout: action })
+    }
   }
 
   return (
