c2e9f67fbb38b9dfaa89f6f2d240a1e1354c7fd1
rdx <reydux95@gmail.com>
2024-11-20 10:24:50 +0100

    feat(logs): add hostname

--- packages/logs/src/index.ts
+++ packages/logs/src/index.ts
@@ -18,7 +18,7 @@ export const initLogger = (
 ) => {
   const logger =
     process.env.NODE_ENV === "development"
-      ? pino({ level: "debug" }, pinoPretty({ ignore: "pid,hostname" }))
+      ? pino({ level: "debug" }, pinoPretty({ ignore: "pid" }))
       : pino(
           { level: "debug", base: null },
           pinoLoki({
