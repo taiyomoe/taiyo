a535dc9790789f90daba89e2f4af97548591fbf7
rdx <reydux95@gmail.com>
2025-02-10 04:03:00 +0100

    fix(settings): navbar overlapping page content

--- apps/web/src/app/(root)/user/settings/layout.tsx
+++ apps/web/src/app/(root)/user/settings/layout.tsx
@@ -1,14 +1,18 @@
 import { Divider } from "@nextui-org/divider"
+import { Navbar } from "~/components/navbar/navbar"
 import type { LayoutProps } from "~/lib/types"
 
 export default async function Layout({ children }: LayoutProps) {
   return (
-    <main className="w-full max-w-7xl place-self-center p-bodyPadding">
-      <div className="flex flex-col">
-        <p className="font-semibold text-4xl">Configurações</p>
-        <Divider className="my-8" />
-        {children}
-      </div>
-    </main>
+    <div className="flex h-full flex-col">
+      <Navbar mode="sticky" showLogo />
+      <main className="w-full max-w-7xl place-self-center p-bodyPadding">
+        <div className="flex flex-col">
+          <p className="font-semibold text-4xl">Configurações</p>
+          <Divider className="my-8" />
+          {children}
+        </div>
+      </main>
+    </div>
   )
 }
