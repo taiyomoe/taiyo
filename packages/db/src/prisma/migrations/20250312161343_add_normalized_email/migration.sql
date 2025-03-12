b568d7c9808677c5aca3f551e5923eb0e9b0c1ff
rdx <reydux95@gmail.com>
2025-03-12 17:31:13 +0100

    feat(db): add data migration for normalized email field

---
+++ packages/db/src/prisma/migrations/20250312161343_add_normalized_email/migration.sql
@@ -0,0 +1,14 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[normalizedEmail]` on the table `User` will be added. If there are existing duplicate values, this will fail.
+  - Made the column `emailVerified` on table `User` required. This step will fail if there are existing NULL values in that column.
+
+*/
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "normalizedEmail" TEXT,
+ALTER COLUMN "emailVerified" SET NOT NULL,
+ALTER COLUMN "emailVerified" DROP DEFAULT;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_normalizedEmail_key" ON "User"("normalizedEmail");
