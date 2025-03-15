52c48131d1327589035c916e86cbc6e8938a7af1
rdx <reydux95@gmail.com>
2025-03-15 21:15:04 +0100

    refactor(auth): remove redundant id from logo links

--- apps/new-web/src/app/auth/error/page.tsx
+++ apps/new-web/src/app/auth/error/page.tsx
@@ -12,7 +12,7 @@ export default async function Page({ searchParams }: Props) {
 
   return (
     <main className="flex min-h-dvh flex-col items-center justify-center gap-8 bg-subtle p-4 md:gap-14 md:py-12 dark:bg-default">
-      <Link id="auth-logo" href="/" className="flex items-center gap-2">
+      <Link href="/" className="flex items-center gap-2">
         <TaiyoLogo className="size-20" />
         <h2 className="font-bold text-4xl tracking-tight">Taiyō</h2>
       </Link>
