d74dfddc69238bce9299299b557d2251b6fc366a
rdx <reydux95@gmail.com>
2025-03-18 23:17:51 +0100

    fix(sidebar): content max height

--- apps/new-web/src/app/(root)/_components/sidebar-content.tsx
+++ apps/new-web/src/app/(root)/_components/sidebar-content.tsx
@@ -8,7 +8,7 @@ export const SidebarContent = () => {
   const t = useTranslations("global")
 
   return (
-    <div className="max-h-[calc(100vh-122px)] overflow-y-auto overflow-x-hidden p-4 transition-[padding] duration-300 group-data-[state=collapsed]:p-2">
+    <div className="max-h-[calc(100vh-202px)] overflow-y-auto overflow-x-hidden p-4 transition-[padding] duration-300 group-data-[state=collapsed]:p-2">
       <SidebarButton href="/" label={t("home")} icon={HomeIcon} />
       <SidebarButton
         href="/dashboard"
