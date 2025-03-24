cb7698e91190b03ed84d7b13252ba8b75d2df06f
rdx <reydux95@gmail.com>
2025-03-24 18:45:30 +0100

    fix(ui): center email sent screens

--- apps/web/src/app/auth/_components/auth-verification-email-sent.tsx
+++ apps/web/src/app/auth/_components/auth-verification-email-sent.tsx
@@ -10,11 +10,11 @@ export const AuthVerificationEmailSent = () => {
   return (
     <div className="space-y-8">
       <BackArrowButton onPress={goToSocials} />
-      <div className="relative flex flex-col items-center justify-center gap-2">
+      <div className="relative flex flex-col items-center justify-center gap-2 text-center">
         <MailSentIllustration className="mb-6 h-fit w-44 sm:w-56" />
         <h1 className="font-bold text-2xl">{t("title")}</h1>
-        <h2 className="text-center text-subtle">{t("description1")}</h2>
-        <p className="text-center text-sm text-subtle">{t("description2")}</p>
+        <h2 className="text-subtle">{t("description1")}</h2>
+        <p className="text-sm text-subtle">{t("description2")}</p>
       </div>
     </div>
   )
