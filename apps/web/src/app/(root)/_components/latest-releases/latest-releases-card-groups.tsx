6790d50d7c3bb51c7febd3df1ff8f723333c2b0c
rdx <reydux95@gmail.com>
2025-04-01 17:27:18 +0200

    feat(home): add loading skeletons to group hover card

--- apps/web/src/app/(root)/_components/latest-releases/latest-releases-card-groups.tsx
+++ apps/web/src/app/(root)/_components/latest-releases/latest-releases-card-groups.tsx
@@ -1,6 +1,8 @@
 import type { Group } from "@taiyomoe/db"
 import { UsersIcon } from "lucide-react"
 import Link from "next/link"
+import { type RefObject, useRef } from "react"
+import { useHover } from "usehooks-ts"
 import { GroupsHoverCardContent } from "~/components/hover-cards/groups-hover-card-content"
 import { HoverCard, HoverCardTrigger } from "~/components/ui/hover-card"
 
@@ -13,19 +15,25 @@ export const LatestReleasesCardGroups = ({ groups }: Props) => {
   return (
     <div className="flex items-center gap-2">
       <UsersIcon className="size-4 min-w-fit" />
-      {groups.map((group) => (
-        <HoverCard key={group.id}>
-          <HoverCardTrigger asChild>
-            <Link
-              href={`/groups/${group.id}`}
-              className="-my-0.5 -mx-1 line-clamp-1 w-fit break-all rounded px-1 py-0.5 transition-colors hover:bg-subtle hover:text-default data-[state=open]:bg-subtle data-[state=open]:text-default"
-            >
-              {group.name}
-            </Link>
-          </HoverCardTrigger>
-          <GroupsHoverCardContent id={group.id} />
-        </HoverCard>
-      ))}
+      {groups.map((group) => {
+        const triggerRef = useRef<HTMLAnchorElement>(null)
+        const isHovered = useHover(triggerRef as RefObject<HTMLElement>)
+
+        return (
+          <HoverCard key={group.id}>
+            <HoverCardTrigger asChild>
+              <Link
+                ref={triggerRef}
+                href={`/groups/${group.id}`}
+                className="-my-0.5 -mx-1 line-clamp-1 w-fit break-all rounded px-1 py-0.5 transition-colors hover:bg-subtle hover:text-default data-[state=open]:bg-subtle data-[state=open]:text-default"
+              >
+                {group.name}
+              </Link>
+            </HoverCardTrigger>
+            <GroupsHoverCardContent id={group.id} enabled={isHovered} />
+          </HoverCard>
+        )
+      })}
     </div>
   )
 }
--- apps/web/src/components/hover-cards/groups-hover-card-content.tsx
+++ apps/web/src/components/hover-cards/groups-hover-card-content.tsx
@@ -12,76 +12,77 @@ import { getLogoUrl } from "~/utils/groups/get-logo-url"
 import { useTRPC } from "~/utils/trpc/react"
 import { GroupsHoverCardContentSkeleton } from "./groups-hover-card-content-skeleton"
 
-type Props = { id: string }
+type Props = { id: string; enabled: boolean }
 
-export const GroupsHoverCardContent = ({ id }: Props) => {
-  const t = useTranslations("global")
+export const GroupsHoverCardContent = ({ id, enabled }: Props) => {
   const trpc = useTRPC()
   const { data, isFetching } = useQuery(
-    trpc.groups.getHoverCardContent.queryOptions(id),
+    trpc.groups.getHoverCardContent.queryOptions(id, { enabled }),
   )
-
-  if (isFetching || !data) {
-    return <GroupsHoverCardContentSkeleton />
-  }
+  const t = useTranslations("global")
 
   return (
     <HoverCardContent className="relative w-90">
-      <div className="-mx-3 -mt-3">
-        <Image
-          src={getBannerUrl(data)}
-          className="max-h-16 min-h-16 select-none object-cover"
-          height={64}
-          width={358}
-          radius="none"
-          alt={`${data.name}'s banner`}
-        />
-        <div className="absolute top-0 h-full max-h-16 w-full bg-[linear-gradient(to_bottom,hsla(var(--background),0.6),hsla(var(--background)))]" />
-      </div>
-      <div className="-mt-6 flex items-end gap-2">
-        <Image
-          src={getLogoUrl(data)}
-          className="max-size-12 min-size-12"
-          width={48}
-          height={48}
-          radius="full"
-          alt={`${data.name}'s logo`}
-        />
-        <Link
-          href={`/groups/${data.id}`}
-          className="line-clamp-1 font-medium text-lg hover:underline"
-        >
-          {data.name}
-        </Link>
-      </div>
-      {data.description && (
-        <ScrollShadow className="mt-3 max-h-15" title={data.description}>
-          {data.description}
-        </ScrollShadow>
+      {(isFetching || !data) && <GroupsHoverCardContentSkeleton />}
+      {data && (
+        <>
+          <div className="-mx-3 -mt-3">
+            <Image
+              src={getBannerUrl(data)}
+              className="max-h-16 min-h-16 select-none object-cover"
+              height={64}
+              width={358}
+              radius="none"
+              alt={`${data.name}'s banner`}
+            />
+            <div className="absolute top-0 h-full max-h-16 w-full bg-[linear-gradient(to_bottom,hsla(var(--background),0.6),hsla(var(--background)))]" />
+          </div>
+          <div className="-mt-6 flex items-end gap-2">
+            <Image
+              src={getLogoUrl(data)}
+              className="max-size-12 min-size-12"
+              width={48}
+              height={48}
+              radius="full"
+              alt={`${data.name}'s logo`}
+            />
+            <Link
+              href={`/groups/${data.id}`}
+              className="line-clamp-1 font-medium text-lg hover:underline"
+            >
+              {data.name}
+            </Link>
+          </div>
+          {data.description && (
+            <ScrollShadow className="mt-3 max-h-15" title={data.description}>
+              {data.description}
+            </ScrollShadow>
+          )}
+          <div className="mt-3 flex justify-between">
+            <p>{t("nChapters", { count: data.chaptersCount })}</p>
+            <div className="flex items-center gap-2">
+              <Link
+                href={`https://discord.gg/${data.discord}`}
+                className="transition-opacity hover:opacity-80 [&_path]:text-black dark:[&_path]:text-white [&_svg]:size-4"
+              >
+                <LinkIcon />
+              </Link>
+              <Link
+                href={`https://discord.gg/${data.discord}`}
+                className="transition-opacity hover:opacity-80 [&_svg]:size-5"
+              >
+                <DiscordLogo shadow={false} />
+              </Link>
+              <Link
+                href={`https://discord.gg/${data.discord}`}
+                className="transition-opacity hover:opacity-80 [&_svg]:size-4"
+              >
+                <XLogo color="inverted" />
+              </Link>
+            </div>
+          </div>
+        </>
       )}
-      <div className="mt-3 flex justify-between">
-        <p>{t("nChapters", { count: data.chaptersCount })}</p>
-        <div className="flex items-center gap-2">
-          <Link
-            href={`https://discord.gg/${data.discord}`}
-            className="transition-opacity hover:opacity-80 [&_path]:text-black dark:[&_path]:text-white [&_svg]:size-4"
-          >
-            <LinkIcon />
-          </Link>
-          <Link
-            href={`https://discord.gg/${data.discord}`}
-            className="transition-opacity hover:opacity-80 [&_svg]:size-5"
-          >
-            <DiscordLogo shadow={false} />
-          </Link>
-          <Link
-            href={`https://discord.gg/${data.discord}`}
-            className="transition-opacity hover:opacity-80 [&_svg]:size-4"
-          >
-            <XLogo color="inverted" />
-          </Link>
-        </div>
-      </div>
     </HoverCardContent>
   )
 }
