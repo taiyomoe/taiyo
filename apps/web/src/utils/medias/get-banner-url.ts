ccad60e2295c8e5fc8994a6c19c0980ce5c3c93c
rdx <reydux95@gmail.com>
2025-04-01 17:03:48 +0200

    refactor(utils): use direct return

--- apps/web/src/utils/medias/get-banner-url.ts
+++ apps/web/src/utils/medias/get-banner-url.ts
@@ -1,3 +1,2 @@
-export const getBannerUrl = (mediaId: string, banner: { id: string }) => {
-  return `https://cdn.taiyo.moe/medias/${mediaId}/banners/${banner.id}.jpg`
-}
+export const getBannerUrl = (mediaId: string, banner: { id: string }) =>
+  `https://cdn.taiyo.moe/medias/${mediaId}/banners/${banner.id}.jpg`
