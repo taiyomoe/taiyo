e39bf0b8a2c3db25da67ceaad3aae7cb0ae8ea0e
rdx <reydux95@gmail.com>
2025-09-15 19:16:28 +0200

    fix(storybook): replace required usage

--- apps/storybook/.storybook/main.ts
+++ apps/storybook/.storybook/main.ts
@@ -5,8 +5,11 @@ import type { StorybookConfig } from "@storybook/nextjs-vite"
  * This function is used to resolve the absolute path of a package.
  * It is needed in projects that use Yarn PnP or are set up within a monorepo.
  */
-const getAbsolutePath = (value: string) =>
-  dirname(require.resolve(join(value, "package.json")))
+const getAbsolutePath = (packageName: string): string =>
+  dirname(import.meta.resolve(join(packageName, "package.json"))).replace(
+    /^file:\/\//,
+    "",
+  )
 
 export default {
   stories: [
