d2cb5d679d7da9836e4f0b6fae947168e015bcd3
vs0usa <reydux95@gmail.com>
2026-06-20 14:52:30 +0200

    feat(web): add paraglidejs

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,14 +1,22 @@
-import { defineConfig } from "vite"
-import { devtools } from "@tanstack/devtools-vite"
-
-import { tanstackStart } from "@tanstack/react-start/plugin/vite"
-
-import viteReact from "@vitejs/plugin-react"
+import { paraglideVitePlugin } from "@inlang/paraglide-js"
 import tailwindcss from "@tailwindcss/vite"
+import { devtools } from "@tanstack/devtools-vite"
+import { tanstackStart } from "@tanstack/react-start/plugin/vite"
+import viteReact from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
 
 const config = defineConfig({
   resolve: { tsconfigPaths: true },
-  plugins: [devtools(), tailwindcss(), tanstackStart(), viteReact()],
+  plugins: [
+    devtools(),
+    tailwindcss(),
+    tanstackStart(),
+    viteReact(),
+    paraglideVitePlugin({
+      project: "./project.inlang",
+      outdir: "./src/paraglide",
+    }),
+  ],
 })
 
 export default config
